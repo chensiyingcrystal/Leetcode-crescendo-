b48aaf5e8d5892c0a645e899865fb85f5ff6ba73
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-03 10:39:36 -0800

    complete 153

--- Day2-Array-five-problems/153-Minimum-rotated.cpp
+++ Day2-Array-five-problems/153-Minimum-rotated.cpp
@@ -10,7 +10,26 @@ public:
    }
 
     int findMin(vector<int>& nums) {
-        
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+
+        int left = 0;
+        int right = nums.size() - 1;
+        int medium = (left + right) / 2;
+
+        while(left < right and (right - left > 1)) {
+            if(nums[left] < nums[medium]) {
+                left = medium;
+            }
+            else {
+                right = medium;
+            }
+
+            medium = (left + right) / 2;
+
+        }
+        return nums[right];
     }
 };
 
