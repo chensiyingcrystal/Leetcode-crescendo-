0a9e79718658745739efaaefc30469905b8cbff6
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-05 10:49:41 -0800

    complete 332

--- Day4-dp-five problems/332-Coinchange.cpp
+++ Day4-dp-five problems/332-Coinchange.cpp
@@ -8,9 +8,47 @@ public:
     Solution() {
 
     }
+    
+    // int coinChange(vector<int>& coins, int amount) {
+    //     vector<int> ans;
+    //     ans.push_back(0);
+    //     int a = 1;
+    //     while (a <= amount) {
+    //         int a_fewest = INT_MAX;
+    //         for (int i = 0; i < coins.size(); ++i) {
+    //             if (a - coins[i] >= 0) {
+    //                 a_fewest = min(a_fewest, ans[a - coins[i]]);
+    //             }
+    //         }
+    //         if (a_fewest != INT_MAX) {
+    //             ans.push_back(a_fewest + 1);
+    //         }
+    //         else {
+    //             ans.push_back(a_fewest);
+    //         }
+    //         a += 1;
+    //     }
+    //     return ans[amount] == INT_MAX ? -1 : ans[amount];
+    // }
+
+//optimized method
     int coinChange(vector<int>& coins, int amount) {
-        
+        vector<int> ans(amount + 1, INT_MAX);
+        ans[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            int min_value = INT_MAX;
+            for (auto coin : coins) {
+                if (i - coin >= 0) {
+                    min_value = min(min_value, ans[i - coin]);
+                }
+            }
+            if (min_value != INT_MAX) {
+                ans[i] = min_value + 1;
+            } 
+        }
+        return ans[amount] == INT_MAX ? -1 : ans[amount];
     }
+
 };
 
 
