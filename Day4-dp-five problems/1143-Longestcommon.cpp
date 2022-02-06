fe9ae42e3958f8fb7216cb5a180b8e40f0272e32
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-05 20:50:33 -0800

    complete 1143

--- Day4-dp-five problems/1143-Longestcommon.cpp
+++ Day4-dp-five problems/1143-Longestcommon.cpp
@@ -11,12 +11,23 @@ public:
     }
 
     int longestCommonSubsequence(string text1, string text2) {
+        const int n = text1.size();
+        const int m = text2.size();
+        vector<int> p(m + 1, 0);
+        vector<vector<int> > longestsub(n + 1, p);
+        for(int i = 1; i < n + 1; ++i) {
+            for(int j = 1; j < m + 1; ++j) {
+                if (text1[i - 1] == text2[j - 1]) {
+                    longestsub[i][j] = longestsub[i - 1][j - 1] + 1;
+                }
+                else {
+                    longestsub[i][j] = max(longestsub[i - 1][j], longestsub[i][j - 1]);
+                }
+            }
+        }
 
+        return longestsub[n][m];
     }
-
-
-
-
 
 int main() {
     string text1 = "abcde";
