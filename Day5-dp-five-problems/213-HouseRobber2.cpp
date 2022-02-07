74afadba2a0b2916d9065d290817336607538e0c
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-07 13:46:21 -0800

    Update 213-HouseRobber2.cpp

--- Day5-dp-five-problems/213-HouseRobber2.cpp
+++ Day5-dp-five-problems/213-HouseRobber2.cpp
@@ -9,19 +9,6 @@ public:
 
     }
 
-//第一遍错误原因（方程定义错误）错误方程： f(n) = f(n - 2) + nums[n], f的定义为一定要包含自身，实际上这个决定应该是自动判决的
-//自动判定是否包含自身的方程并不会导致重复，正确方程 f(n) = max(f(n - 2) + nums[n], f[n - 1])
-//计算index太慢，正确应该是以rob的i为主，再换算成为nums的j
-    int rob(vector<int>& nums) {
-        const int n = nums.size();
-        vector<int> rob(n + 2);
-        rob[0] = 0;
-        rob[1] = 0;
-        for(int i = 2; i < n + 2; ++i) {
-            rob[i] = max(rob[i - 2] + nums[i - 2], rob[i - 1]);
-        }
-        return rob[n + 1];   
-    }
 
 
 }
