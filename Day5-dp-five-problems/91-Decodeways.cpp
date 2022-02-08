2c34b76c51bf9515ae60c32af43a22390480a799
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-08 08:20:54 -0800

    Create 91-Decodeways.cpp

---
+++ Day5-dp-five-problems/91-Decodeways.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    Solution() {
+
+    }
+
+    int numDecodings(string s) {
+        
+    }
+
+
+
+
+}
+
+int main() {
+    string s = "126";
+    Solution* ss = new Solution();
+    int result = ss->numDecodings(s);
+    cout << result << endl;
+    return 0;
+}
