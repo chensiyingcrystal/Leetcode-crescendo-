e2ea95d361dd6dfd77e48baf8dddf060926a309b
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-10 13:36:53 -0800

    Create 57-InsertInterval.cpp

---
+++ Day8-Interval-five-question/57-InsertInterval.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+   Solution() {
+
+   }
+
+    vector<vector<int> > insert(vector<vector<int> >& intervals, vector<int>& newInterval) {
+        
+        
+    }
+
+};
+
+
+int main() {
+    return 0;
+}
