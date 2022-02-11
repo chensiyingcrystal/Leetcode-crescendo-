7a4cb2e7cc025063ac183bd8ed094bda1c122d96
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-11 10:48:28 -0800

    Create 206-ReverseLinkedList.cpp

---
+++ Day9-LinkedList-five-problems/206-ReverseLinkedList.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+   Solution() {
+
+
+   }
+
+    struct ListNode {
+        int val;
+        ListNode *next;
+        ListNode() : val(0), next(nullptr) {}
+        ListNode(int x) : val(x), next(nullptr) {}
+        ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+
+    ListNode* reverseList(ListNode* head) {
+        // special case
+        if (head == NULL) return NULL;
+        ListNode* curr = new ListNode();
+        ListNode* prev = new ListNode();
+        ListNode* next = new ListNode();
+        curr = head;
+        prev = NULL;
+        next = NULL;
+        while (curr->next != NULL) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        curr->next = prev;
+        return curr; 
+    } 
+
+
+};
+
+
+int main() {
+    return 0;
+}
