1e3f5d44a312e40a9229714290d1a9260ce05b93
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-11 11:36:33 -0800

    Update 141-LinkedListCycle.cpp

--- Day9-LinkedList-five-problems/141-LinkedListCycle.cpp
+++ Day9-LinkedList-five-problems/141-LinkedListCycle.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
@@ -15,9 +16,29 @@ public:
         ListNode *next;
         ListNode(int x) : val(x), next(NULL) {}
     };
-
+// space complexity o(n), time o(n)
     bool hasCycle(ListNode *head) {
-        
+        if (head == NULL) return false;
+        unordered_set<ListNode*> visited;
+        ListNode* curr = head;
+        while(curr != NULL) {
+            if (visited.find(curr->next) != visited.end()) return true;
+            visited.insert(curr);
+            curr = curr->next;
+        }
+        return false;
+    }
+
+// space can be o(1)
+    bool hasCycle(ListNode *head) {
+        if (head == NULL) return false;
+        ListNode* curr = head;
+        while(curr != NULL) {
+            if (curr->val == INT_MIN) return true;
+            curr->val = INT_MIN;
+            curr = curr->next;
+        }
+        return false;
     }
 
 };
