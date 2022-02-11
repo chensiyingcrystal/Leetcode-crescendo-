43d5205195b4d31165c67bcc54538275af005928
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-11 10:51:47 -0800

    Create 141-LinkedListCycle.cpp

---
+++ Day9-LinkedList-five-problems/141-LinkedListCycle.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+   Solution() {
+
+
+   }
+
+    struct ListNode {
+        int val;
+        ListNode *next;
+        ListNode(int x) : val(x), next(NULL) {}
+    };
+
+    bool hasCycle(ListNode *head) {
+        
+    }
+
+};
+
+
+int main() {
+    return 0;
+}
