a67390f58d81fece24df71ca4b0f455df28a57ef
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-11 11:53:28 -0800

    Create 21-MergeTwoSortedLists.cpp

---
+++ Day9-LinkedList-five-problems/21-MergeTwoSortedLists.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+   Solution() {
+
+
+   }
+
+    struct ListNode {
+        int val;
+        ListNode *next;
+        ListNode() : val(0), next(nullptr) {}
+        ListNode(int x) : val(x), next(nullptr) {}
+        ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        
+    }
+
+
+};
+
+
+int main() {
+    return 0;
+}
