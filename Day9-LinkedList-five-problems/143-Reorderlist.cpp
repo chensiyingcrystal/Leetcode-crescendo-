bcf20eaf1a5f902ef9ca508dd8fa7e70be87ffd4
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-12 18:36:53 -0800

    complete 143

--- Day9-LinkedList-five-problems/143-Reorderlist.cpp
+++ Day9-LinkedList-five-problems/143-Reorderlist.cpp
@@ -17,9 +17,46 @@ public:
         ListNode(int x) : val(x), next(NULL) {}
     };
 
-    ListNode* reverseList(ListNode* head) {
+    void reorderList(ListNode* head) {
+        if (head == NULL) return;
+        // find middle 
+        ListNode* slow = head; 
+        ListNode* fast = head;
+        while(fast->next != NULL && fast->next->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        // split and reverse the second part
+        // important!!! completely split, avoid cycling
+        // 两部分需要完全断开，长端在前
+        ListNode* prev = NULL;
+        ListNode* curr = slow->next;
+        slow->next = NULL;
+        ListNode* next;
+        
+        while(curr != NULL) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        // merge two part
+        ListNode* first = head;
+        ListNode* second = prev;
+       
+        ListNode* tmp;
+        // important decision!!!
+        // 以短端作为约束条件
+        while(second != NULL) {
+            tmp = first->next;
+            first->next = second;
+            first = tmp;
+            tmp = second->next;
+            second->next = first;
+            second = tmp;
+        }
     }
-
 };
 
 
