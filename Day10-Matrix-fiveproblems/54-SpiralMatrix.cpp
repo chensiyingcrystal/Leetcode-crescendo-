36a7fcdb60d459641a45f83d4e588c67798f3e37
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-13 00:43:15 -0800

    complete54

---
+++ Day10-Matrix-fiveproblems/54-SpiralMatrix.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+   Solution() {
+
+
+   }
+
+    vector<int> spiralOrder(vector<vector<int> >& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<int> spiral;
+        int start_r = 0;
+        int start_c = 0;
+        while(m > 0 && n > 0) {
+            for(int j = 0; j < n; j++) spiral.push_back(matrix[start_r][start_c + j]);
+            for(int i = 1; i < m; i++) spiral.push_back(matrix[start_r + i][start_c + n - 1]);
+            if (m >= 2) {
+                for(int j = n - 2; j >= 0; j--) spiral.push_back(matrix[start_r + m - 1][start_c + j]);
+            }
+            if (n >= 2) {
+                for(int i = m - 2; i >= 1; i--) spiral.push_back(matrix[start_r + i][start_c]);
+            }
+            m = m - 2;
+            n = n - 2;
+            start_r += 1;
+            start_c += 1;
+        }
+        return spiral;
+    }
+
+        vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<int> spiral;
+        int start_r = 0;
+        int start_c = 0;
+        while(m > 0 && n > 0) {
+            for(int j = start_c; j <= start_c + n - 1; j++) spiral.push_back(matrix[start_r][j]);
+            for(int i = 1 + start_r; i <= start_r + m - 1; i++) spiral.push_back(matrix[i][start_c + n - 1]);
+            if (m >= 2) {
+                for(int j = start_c + n - 2; j >= start_c; j--) spiral.push_back(matrix[start_r + m - 1][j]);
+            }
+            if (n >= 2) {
+                for(int i = start_r + m - 2; i >= 1 + start_r; i--) spiral.push_back(matrix[i][start_c]);
+            }
+            m = m - 2;
+            n = n - 2;
+            start_r += 1;
+            start_c += 1;
+        }
+        return spiral;
+    }
+
+
+};
+
+
+int main() {
+    return 0;
+}
--- Day10-Matrix-fiveproblems/73-SetMatrixZeroes.cpp
+++ Day10-Matrix-fiveproblems/73-SetMatrixZeroes.cpp
@@ -10,8 +10,57 @@ public:
 
    }
 
-    void setZeroes(vector<vector<int>>& matrix) {
+    void setZeroes(vector<vector<int> >& matrix) {
+        if (matrix.empty()) return;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int cross = 1;
+
+        // loop to find zero and set 
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    if (i == 0) cross = 0;
+                    else matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // priority: main part(i = 1 to m-1, j = 1 to n-1) -> zero column(j=0) -> zero row(i =0)
+        // loop through marked row and column and set 
+        // if use auto remember to put &!! 否则修改的只是x的值！！！
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        // loop through zero column
+        if (matrix[0][0] == 0) {
+            for(int i = 1; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        // set zero row
+        if (cross == 0) {
+            for (int j = 1; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // optimized edition: traversing in the diverse direction
+        for(int j = n - 1; j >= 0; --j) {
+            for (int i = m - 1; i >= 1; --i) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+            if (cross == 0) matrix[0][j] = 0;
+        }
         
+        return;
     }
 
 
