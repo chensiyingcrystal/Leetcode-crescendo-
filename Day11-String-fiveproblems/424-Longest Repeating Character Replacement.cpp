c06fedf62666c3c1f99f23a3a48d97546436fd91
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-14 16:38:32 -0800

    Create 424-Longest Repeating Character Replacement.cpp

---
+++ Day11-String-fiveproblems/424-Longest Repeating Character Replacement.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    Solution() {
+
+
+    }
+
+    int characterReplacement(string s, int k) {
+        
+    }
+};
