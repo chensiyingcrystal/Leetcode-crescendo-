96fcd97da77db27d033bc3a4eee75a234d61e763
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-15 12:54:13 -0800

    Update 269-AlienDictionary.cpp

--- Day7-Graph-five-problems/269-AlienDictionary.cpp
+++ Day7-Graph-five-problems/269-AlienDictionary.cpp
@@ -12,59 +12,85 @@ public:
 
     }
 
-    string AlienDictionary(vector<string>& words) {
+    string alienOrder(vector<string>& words) {
         // special case
         if (words.empty()) return "";
         if (words.size() == 1) return words[0];
         // 构建拓扑图
         unordered_map<char, unordered_set<char> > topo;
         unordered_map<char, int> restrictions;
-        Buildgraph(words, topo, restrictions);
-        // 存储结果和指示器
+        // 构建存储结果和指示器
         string result;
+        for (auto& word : words) {
+            for (auto& c : word) {
+                restrictions[c] = 0;
+            }
+        }
         int number = restrictions.size();
+        // 构建拓扑图        
+        if (!buildGraph(words, topo, restrictions)) return "";
         // 初始化队列：确定初始元素
         queue<char> q;
-        for(auto& c : restricions) {
-            if (!restrictions[c]) {
-                q.push(c);
-                result += c;
+        for(auto c : restrictions) {
+            if (c.second == 0) {
+                q.push(c.first);
+                result.push_back(c.first);
             }
         }
         // 开始拓扑排序（bfs）
         while(!q.empty()) {
-            char smaller = q.front();
+            char cur = q.front();
             q.pop();
             number --;
-            for(auto& neighbor : topo[smaller]) {
+            for(auto& neighbor : topo[cur]) {
                 restrictions[neighbor] --;
-                if (!restrictions[neighbor]) {
+                if (restrictions[neighbor] == 0) {
                     q.push(neighbor);
-                    result += neighbor;
+                    result.push_back(neighbor);
                 }
-
             }
         }
         return number == 0? result : "";
     }
 
-    
-    void Buildgraph(vector<string>& words, unordered_map<char, unordered_set<char> >& topo, 
+    bool buildGraph(vector<string>& words, unordered_map<char, unordered_set<char> >& topo, 
     unordered_map<char, int>& restrictions) {
-         for(int i = 0; i < words.size(); i++) {
-            for(int j = i + 1; j < words.size(); j++) {
-                int min_length = min(words[i].size(), words[j].size());
-                for(int x = 0; x < min_length; x++) {
-                    if (words[i][x] != words[j][x]) {
-                        if (topo[words[i][x]].find(words[j][x]) == topo[words[i][x]].end()) {
-                            topo[words[i][x]].insert(words[j][x]);
-                            restrictions[words[j][x]] += 1;
-                        }
+        for(int i = 0; i < words.size() - 1; i++) {
+            string self = words[i];
+            string next = words[i + 1];
+            // 检查是否会出现ab abb的情况
+            if(self.size() < next.size() && next.substr(0, self.size()) == self) return false;
+            for (int j = 0; j < min(self.size(), next.size()); j++) {
+                if (self[j] != next[j]) {
+                    if (topo[self[j]].find(next[j]) == topo[self[j]].end()) {
+                        topo[self[j]].insert(next[j]);
+                        restrictions[next[j]] += 1;
                     }
-                } 
+                    break;
+                }
             }
-         }
+        }
+        return true;
     }
+
+    // 为什么只用比较相邻的？
+    // void buildGraph(vector<string>& words, unordered_map<char, unordered_set<char> >& topo, 
+    // unordered_map<char, int>& restrictions) {
+    //      for(int i = 0; i < words.size(); i++) {
+    //         for(int j = i + 1; j < words.size(); j++) {
+    //             int min_length = min(words[i].size(), words[j].size());
+    //             for(int x = 0; x < min_length; x++) {
+    //                 if (words[i][x] != words[j][x]) {
+    //                     if (topo[words[i][x]].find(words[j][x]) == topo[words[i][x]].end()) {
+    //                         topo[words[i][x]].insert(words[j][x]);
+    //                         restrictions[words[j][x]] += 1;
+    //                     }
+    //                     break;
+    //                 }
+    //             } 
+    //         }
+    //      }
+    // }
 };
 
 int main() {
