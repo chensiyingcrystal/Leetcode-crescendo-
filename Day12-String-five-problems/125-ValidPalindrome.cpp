10d9ac70cab68aa2f7bc0a8f70594bd962636a0a
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-17 15:20:43 -0800

    Update 125-ValidPalindrome.cpp

--- Day12-String-five-problems/125-ValidPalindrome.cpp
+++ Day12-String-five-problems/125-ValidPalindrome.cpp
@@ -13,25 +13,44 @@ public:
 
 
     }
+//easy solution: reverse and compare, little trick: sift all alnum
+  bool isPalindrome(string s) {
+    string filtered_string, reversed_string;
 
-    bool isValid(string s) {
-        stack<char> str;
-        
+//filtering
+    for (auto ch : s) {
+    //use isalnum to determine if it's an Alphanumeric character
+      if (isalnum(ch))
+        filtered_string += tolower(ch);
+    }
+
+//reversing
+    reversed_string.resize(filtered_string.size());
+    reverse_copy(filtered_string.begin(), filtered_string.end(),
+                 reversed_string.begin());
+
+//comparing
+    return filtered_string == reversed_string;
+  }
+
+//常规做法（自己写的）
+    bool isPalindrome(string s) {
+        string t;
         for (char c : s) {
-            if (c == '(' || c == '[' || c == '{') {
-                str.push(c);
-            }
-
-            else{
-                //important, easy to be bug, test order
-                if (str.empty()) return false;
-                char p = str.top();
-                if ((c == ')' && p == '(') || (c == ']' && p == '[') || (c == '}' && p == '{')) str.pop();
-                else return false;
-            }
+            if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || 
+                (c >= 'A' && c <= 'Z')) {
+                    //加32 而不是加‘a’，加‘a’是转换为int
+                    if (c >= 'A' && c <= 'Z') c += 32; 
+                    t += c;
+                }
         }
-        //important, test counting
-        return str.empty();
+        
+        if (t.empty()) return true;
+        const int n = t.size();
+        for (int i = 0; i < n/2; i++) {
+            if (t[i] != t[n - 1 - i]) return false;
+        }
+        return true;
     }
 
 };
