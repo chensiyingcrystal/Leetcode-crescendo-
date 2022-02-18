300b79a2c5e4ef399ef0a3ea3dbc253c427e71e6
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-17 22:46:58 -0800

    Update 647-PalindromicSubstrings.cpp

--- Day12-String-five-problems/647-PalindromicSubstrings.cpp
+++ Day12-String-five-problems/647-PalindromicSubstrings.cpp
@@ -11,8 +11,23 @@ public:
 
 
     }
-    
+
     int countSubstrings(string s) {
-        
+        const int n = s.size();
+        int sum = 0;
+        auto getNumber = [&](int left, int right) {
+            int count = 1;
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                left--;
+                right++;
+                count++;
+            }
+            return count;
+        };
+        for (int i = 0; i < n; i++) {
+            int num = getNumber(i, i) + getNumber(i, i + 1);
+            sum += num;
+        }
+        return sum;
     }
 };
