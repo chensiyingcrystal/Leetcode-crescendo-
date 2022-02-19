090921796d6358272c3249eb419196f16740898d
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-18 17:26:22 -0800

    Update 124-BinaryTreeMaximumPathSum.cpp

--- Day13-Tree-fivequestions/124-BinaryTreeMaximumPathSum.cpp
+++ Day13-Tree-fivequestions/124-BinaryTreeMaximumPathSum.cpp
@@ -19,7 +19,11 @@ public:
         TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
         TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
     };
-
+//add another value besides the answer
+//for every node, the max value is ans = max(ans, x + max(x.left, 0) + max(x.right, 0))
+//This expression can handle every case, including only left or right, because it is updated inside the function
+//The other value is M(x) = x + max(max(left, 0), max(right, 0))
+//This value is set as their maximum value to pass on to its upper structure
     int maxPathSum(TreeNode* root) {
         if (!root) return 0;
         int ans = INT_MIN;
@@ -31,6 +35,8 @@ public:
         if (!node) return 0;
         int maxLeft = max(maxCurrsum(node -> left, ans), 0);
         int maxRight = max(maxCurrsum(node -> right, ans), 0);
+        //add &ans to this function to make sure that ans is updated on every node's case
+        //including the case in which the maximum value is from the single, isolated node
         ans = max(ans, node -> val + maxLeft + maxRight);
         return node -> val + max(maxLeft, maxRight);
     }
