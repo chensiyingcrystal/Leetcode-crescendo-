1cac42cb82906b8a05952d684ed6f89ed1fbe90e
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-19 14:22:56 -0800

    Update 297-Serialize-and-Desiralize.cpp

--- Day14-Tree-fiveproblems/297-Serialize-and-Desiralize.cpp
+++ Day14-Tree-fiveproblems/297-Serialize-and-Desiralize.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <unordered_map>
 #include <queue>
+#include <sstream>
 
 using namespace std;
 
@@ -30,20 +31,53 @@ public:
             TreeNode* tmp = q.front();
             q.pop();
             
-            if (!tmp) result += '#';
-            else result += to_string(tmp -> val);
-
-            if (tmp) {
-                q.push(tmp -> left);
-                q.push(tmp -> right);
+            if (!tmp) {
+            //bug: use space to split words, since not all value are from 0-9
+                result += "# ";
+                continue;
             }
+            //add space to split
+            else result += to_string(tmp -> val) + " ";
+
+            q.push(tmp -> left);
+            q.push(tmp -> right);
         }
         return result;
     }
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        
+        if (data == "") return NULL;
+
+        //tricks: use stringsteam to split string against spaces
+        vector<string> splitData;
+        stringstream ss(data);
+        string word;
+        while (ss >> word) {
+            splitData.push_back(word);
+        }
+
+        //begin bfs
+        queue<TreeNode*> q;
+        //trick: use stoi to convert string to int
+        TreeNode* root = new TreeNode(stoi(splitData[0], 0, 10));
+        q.push(root);
+
+        for (int i = 1; i < splitData.size(); i += 2) {
+            TreeNode* node = q.front();
+            q.pop();
+            
+            //bug: splitData's elements are string, must extract its element, which is character
+            if (splitData[i][0] != '#') {
+                node -> left = new TreeNode(stoi(splitData[i], 0, 10));
+                q.push(node -> left);
+            }
+            if (splitData[i + 1][0] != '#') {
+                node -> right = new TreeNode(stoi(splitData[i + 1], 0, 10));
+                q.push(node -> right);
+            }
+        }
+        return root;
     }
 
 
