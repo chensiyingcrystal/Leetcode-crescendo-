59ee4c150ce22543229c08ed3bf426edf3707ef2
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-20 17:10:50 -0800

    Update 230-kthSmallest.cpp

---
+++ Day14-Tree-fiveproblems/230-kthSmallest.cpp
@@ -0,0 +1,29 @@
+#include <unordered_map>
+#include <queue>
+#include <sstream>
+
+using namespace std;
+
+class Solution {
+public:
+    Solution() {
+
+
+    }
+
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+
+    int kthSmallest(TreeNode* root, int k) {
+        
+    }
+
+
+
+};
