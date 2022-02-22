6c67c29a24953f6753dd15b7e4e7e6fe0d3e418f
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-22 11:30:40 -0800

    create 135

---
+++ Day17-Greeding-tag-fourquestions/135-Candy.cpp
@@ -0,0 +1,16 @@
+#include <unordered_map>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    Solution() {
+
+
+    }
+    int candy(vector<int>& ratings) {
+        
+    }
+ 
+};
--- Day17-Greeding-tag-fourquestions/455-Assigncookies.cpp
+++ Day17-Greeding-tag-fourquestions/455-Assigncookies.cpp
@@ -10,20 +10,50 @@ public:
 
     }
 //solution: the child with minimum greed factor get smallest cookies first
+//问题：想不出思路；只会用暴力解法；代码错误
     int findContentChildren(vector<int>& g, vector<int>& s) {
+        //排序是常见的预处理操作，时间复杂度o(nlogn)
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
+        int count = 0;
 
         unordered_map<int, int> visited;
         for (auto& x : s) {
             visited[x] = 0;
         }
+//问题：不知道如何标记该饼干已被拿走或者不要重新访问，减少时间复杂度
+//解决：既然已经排好序了，i和j应该一起移动
+        for (auto i : g) {
+            for (auto j : s) {
+                if (j >= i && visited[j] != 1) {
+                    count += 1;
+                    //错误原因：用s的元素而不是index表示，s中元素会重复！！
+                    //用map或者set时先考虑元素是不是重复
+                    visited[j] = 1;
+                    //bug：边界条件控制不对，没有考虑如何进入下轮循环
+                    break；
+                }
+            }
+        }
 
+        return count;
+    }
 
+//正确代码
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
 
-        
-
-        
+        int count = 0;
+        int i = 0, j = 0;
+        while (i < g.size() && j < s.size()) {
+            if (g[i] <= s[j]) {
+                i++;
+                count++;
+            }
+            j++;
+        }
+        return count;
     }
  
 };
