818567209b3e3ecb699e5cfba8ff49a2347701f9
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-23 23:24:43 -0800

    Create 122-BestTImetoBuyandSellStock.cpp

---
+++ Day18-Greedy-three-questions/122-BestTImetoBuyandSellStock.cpp
@@ -0,0 +1,28 @@
+#include <unordered_map>
+#include <bits/stdc++.h>
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+class Solution {
+public:
+    Solution() {
+
+
+    }
+//这里能运用贪心，是因为股票可以当天买卖，每天最大化利益，局部最优 -> 全局最优
+    int maxProfit(vector<int>& prices) {
+        int ans = 0; 
+        int prev = prices[0];
+        //bug: int i
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] > prev) {
+                ans += prices[i] - prev;
+            }
+            prev = prices[i];
+        }
+        return ans;
+    }
+
+};
