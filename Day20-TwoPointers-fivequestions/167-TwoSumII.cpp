df2cd4a1b9245e976c373ff417c5ff5ba77234e4
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-25 12:14:30 -0800

    update

---
+++ Day20-TwoPointers-fivequestions/167-TwoSumII.cpp
@@ -0,0 +1,52 @@
+#include <unordered_map>
+#include <bits/stdc++.h>
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+class Solution {
+public:
+    Solution() {
+
+
+    }
+    
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        const int k = numbers.size();
+        vector<int> result;
+        int i = 0, j = k - 1;
+        while (i < k - 1 && j > 0 && i < j) {
+            int ans = numbers[i] + numbers[j];
+            if (ans == target) {
+                //code optimization
+                return {i + 1, j + 1};
+                // result.push_back(i + 1);
+                // result.push_back(j + 1);
+                // i++;
+                // j--;
+            }
+            else if (ans > target) {
+                j--;
+            }
+            else {
+                i++;
+            }
+        }
+        //在if语句中return的，要保证函数在外面也有return值
+        return {-1, -1};
+        // return result;
+    }  
+    
+//更简洁的写法
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0, r = numbers.size() - 1, sum;
+        while (l < r) {
+            sum = numbers[l] + numbers[r];
+            if (sum == target) break;
+            if (sum > target) r--;
+            else ++l;
+        }
+        return {l + 1, r + 1};
+    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         )
+};
---
+++ Day20-TwoPointers-fivequestions/88-MergeSortedArray.cpp
@@ -0,0 +1,39 @@
+#include <unordered_map>
+#include <bits/stdc++.h>
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+class Solution {
+public:
+    Solution() {
+
+
+    }
+//最初：不会解，卡壳原因：while的边界条件怎么定义？
+    void merger(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> mer(m + n, 0);
+        int i = 0, j = 0;
+        while (i < m && j < n) {
+            if (nums1[i] <= nums2[j]) {
+                mer.push_back(nums1[i]);
+                i++;
+            }
+            else {
+                mer.push_back(nums2[j]);
+                j++;
+            }
+        }
+        if (i == 0 && m == 0) {
+            
+        }
+    } 
+//取巧的办法（没有用双指针）：merger and sort
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for (int i = 0; i < n; i++) {
+            nums1[i + m] = nums2[i];
+        }
+        sort(nums1.begin(), nums1.end());
+    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  )
+};
