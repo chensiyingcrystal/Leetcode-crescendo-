caa176e8ea7ec199ddbf403412635b92ef666538
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-24 21:23:40 -0800

    update

--- Day19-Greedy-threequestions/406-QueueReconstruction.cpp
+++ Day19-Greedy-threequestions/406-QueueReconstruction.cpp
@@ -34,4 +34,37 @@ public:
         }
         return result;
     }
+
+//数据结构知识：动态数组底层是用普通数组实现的
+//动态数组有两个大小，一个是size，就是我们调用size函数得到的值，一个是capacity，是不暴露在外的
+//当插入元素后超过capacity时，会自动开辟一片两倍于原先的内存
+//将原有元素一一拷贝过来后，再释放原数组内存
+//因此上面的方法虽然用动态数组实现表面复杂度是o(n2)，但实际上远远不止，应该是o(n2+t*n),其中t是全量拷贝的次数
+//那如果提前对动态数组定义好容量空间，再用insert，insert还是同样的实现方法
+//那如果提前定义好空间，而不用insert呢？手动模拟插入操作呢？实现的效率还是没有链表高效
+
+
+//用链表实现，可以极大地缩短时间，虽然表面上时间复杂度还是o(nlogn + n2) (advance需要o(n))
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(), people.end(), [](vector<int>& a, vector<int>&b) {
+            if (a[0] == b[0]) {
+                return a[1] < b[1];
+            }
+            else return a[0] > b[0];
+        });
+
+        list<vector<int>> q;
+        for (int i = 0; i < people.size(); i++) {
+            list<vector<int>>::iterator it = q.begin();
+            advance(it, people[i][1]);
+            //list唯一费时的是寻找插入的位置，这一步也可以用while循环实现
+            // int position = people[i][1];
+            // while (position--) {
+            //     it++;
+            // }
+            q.insert(it, people[i]);
+        }
+        //如何将链表转换为动态数组
+        return vector<vector<int>> (q.begin(), q.end());
+    }
 };
