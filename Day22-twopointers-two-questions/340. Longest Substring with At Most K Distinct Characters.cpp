4cc1fd1b548ff7ff3e11ea4d7cceb87efce9bc4e
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-28 12:06:27 -0800

    Create 340. Longest Substring with At Most K Distinct Characters.cpp

---
+++ Day22-twopointers-two-questions/340. Longest Substring with At Most K Distinct Characters.cpp
@@ -0,0 +1,28 @@
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <string.h>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    Solution() {
+
+
+    }
+
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
+        int left = 0, right = 0;
+        int count = 0;
+        unordered_set<char> unique;
+        while (right < s.length()) {
+            if (s[])
+
+        }
+        
+    }
+
+
+};
