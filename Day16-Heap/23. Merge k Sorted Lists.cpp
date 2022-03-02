a51db808732ba0820d6e6247dbf244a2cbb56eef
Siying Chen <chensiyingcrystal@gmail.com>
2022-03-02 10:45:27 -0800

    Update 23. Merge k Sorted Lists.cpp

--- Day16-Heap/23. Merge k Sorted Lists.cpp
+++ Day16-Heap/23. Merge k Sorted Lists.cpp
@@ -22,9 +22,33 @@ public:
     };
 
 //Solution1: 在想不出最佳解法下的暴力解法：brute force
-    ListNode* mergeKlists(vector<ListNode*>& lists) {
+//time: o(NlogN), N is the total number of nodes
+//space: o(N), sorting cost o(N) space, creating a new linked list cost o(N) space
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        //traversing through the lists and record the value of node
+        vector<int> new_lists;
+        for (int i = 0; i < lists.size(); i++) {
+            ListNode* tmp = lists[i];
+            while (tmp != nullptr) {
+                new_lists.push_back(tmp -> val);
+                tmp = tmp -> next;
+            }
+        }
         
+        //consider special case here, for example: lists = [[], []]
+        if (new_lists.empty()) return {};
 
+        //sort the new lists in non-decreasing order and make a new linked-lists based on that
+        sort(new_lists.begin(), new_lists.end());
+        ListNode* head = new ListNode(new_lists[0]);
+        ListNode* prev = head;
+        for (int i = 1; i < new_lists.size(); i++) {
+            ListNode* curr = new ListNode(new_lists[i]);
+            prev -> next = curr;
+            prev = curr;
+        }
+
+        return head;
     }
 
 };
