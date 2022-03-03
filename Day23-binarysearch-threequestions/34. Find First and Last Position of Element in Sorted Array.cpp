dc6f7a8ad24d5f8b7d9776b3908d57382a355638
Siying Chen <chensiyingcrystal@gmail.com>
2022-03-03 10:04:05 -0800

    Update 34. Find First and Last Position of Element in Sorted Array.cpp

--- Day23-binarysearch-threequestions/34. Find First and Last Position of Element in Sorted Array.cpp
+++ Day23-binarysearch-threequestions/34. Find First and Last Position of Element in Sorted Array.cpp
@@ -14,19 +14,44 @@ public:
     }
     vector<int> searchRange(vector<int>& nums, int target) {
         if (nums.empty()) return {-1, -1};
-        const int n = nums.size();
-        int low = 0, high = n - 1;
-        vector<int> ans;
-        while (high - low > 1) {
-            int half = (low + high) / 2;
-            if (nums[half] == target) {
-                ans.push_back(half);
-                low = (low + half) / 2;
-                high = (half + high) / 2;
-            }
-            else if (nums[half] > target) high = half;
-            else low = half;
-        }  
+        return {findFirst(nums, target), findLast(nums, target)};
+    }
 
+    int findFirst(vector<int>& nums, int& target) {
+        const int n = nums.size();
+        int low = 0, high = n - 1, leftmost = -1;
+        while (low <=high) {
+            int half = (low + high) / 2;
+            if (nums[half] > target) {
+                high = half - 1;
+            }
+            else if (nums[half] < target) {
+                low = half + 1;
+            }
+            else {
+                leftmost = half;
+                high = half - 1;
+            }
+        }
+        return leftmost;
+    }
+
+    int findLast(vector<int>& nums, int& target) {
+        const int n = nums.size();
+        int low = 0, high = n - 1, rightmost = -1;
+        while (low <= high) {
+            int half = (low + high) / 2;
+            if (nums[half] > target) {
+                high = half - 1;
+            }
+            else if (nums[half] < target) {
+                low = half + 1;
+            }
+            else {
+                rightmost = half;
+                low = half + 1;
+            }
+        }
+        return rightmost;
     }
 };
