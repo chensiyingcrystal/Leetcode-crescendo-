a4c196d3297f7ac8eaf8261f08488f11a7a1a9a6
Siying Chen <chensiyingcrystal@gmail.com>
2022-03-02 18:02:46 -0800

    Update 34. Find First and Last Position of Element in Sorted Array.cpp

--- Day23-binarysearch-threequestions/34. Find First and Last Position of Element in Sorted Array.cpp
+++ Day23-binarysearch-threequestions/34. Find First and Last Position of Element in Sorted Array.cpp
@@ -13,6 +13,20 @@ public:
 
     }
     vector<int> searchRange(vector<int>& nums, int target) {
-        
+        if (nums.empty()) return {-1, -1};
+        const int n = nums.size();
+        int low = 0, high = n - 1;
+        vector<int> ans;
+        while (high - low > 1) {
+            int half = (low + high) / 2;
+            if (nums[half] == target) {
+                ans.push_back(half);
+                low = (low + half) / 2;
+                high = (half + high) / 2;
+            }
+            else if (nums[half] > target) high = half;
+            else low = half;
+        }  
+
     }
 };
