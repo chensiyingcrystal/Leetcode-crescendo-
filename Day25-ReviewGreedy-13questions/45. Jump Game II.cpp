0df49864fc6f647925031187e60dbfc604dac004
Siying Chen <chensiyingcrystal@gmail.com>
2022-03-08 14:09:49 -0800

    Update 45. Jump Game II.cpp

--- Day25-ReviewGreedy-13questions/45. Jump Game II.cpp
+++ Day25-ReviewGreedy-13questions/45. Jump Game II.cpp
@@ -5,6 +5,17 @@ using namespace std;
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        
+        if (nums.size() == 1) return 0;
+        int step = 1, max_cover = nums[0], next_max_cover = 0;
+        int i = 1; 
+        while (i < nums.size()) {
+            if (i > max_cover) {
+                max_cover = next_max_cover;
+                step++;
+            }
+            next_max_cover = max(next_max_cover, i + nums[i]);
+            i++;
+        }
+        return step;
     }
 };
