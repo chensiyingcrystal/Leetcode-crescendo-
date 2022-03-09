0aa701e75c625c973b45ee32eb419ffd435d4f7a
Siying Chen <chensiyingcrystal@gmail.com>
2022-03-08 21:36:54 -0800

    Update 524. Longest Word in Dictionary through Deleting.cpp

--- Day27-TwoPointers-review-sixquestions/524. Longest Word in Dictionary through Deleting.cpp
+++ Day27-TwoPointers-review-sixquestions/524. Longest Word in Dictionary through Deleting.cpp
@@ -7,6 +7,28 @@ using namespace std;
 class Solution {
 public:
     string findLongestWord(string s, vector<string>& dictionary) {
-        
+        //loop through dictionary, to check each word with string
+        string ans = "";
+        for (int i = 0; i < dictionary.size(); i++) {
+            //initialize two pointer, p for the word in dictionary, q for string s
+            int p = 0, q = 0;
+            while (q < s.length()) {
+                if (dictionary[i][p] == s[q]) {
+                    p++;
+                    q++;
+                }
+                q++;
+            }
+            
+            //check if we found the word matches with string s 
+            if (p >= dictionary[i].length()) {
+                if (dictionary[i].length() > ans.length() || dictionary[i] < ans) {
+                    ans = dictionary[i];
+                }
+            }
+        }
+
+        return ans == ""? "" : ans;
+
     } 
 };
