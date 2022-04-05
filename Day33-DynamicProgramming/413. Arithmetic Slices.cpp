01e75f298a6f72c399092717e373e279cac541b5
Siying Chen <chensiyingcrystal@gmail.com>
2022-04-05 10:54:01 -0700

    Update 413. Arithmetic Slices.cpp

--- Day33-DynamicProgramming/413. Arithmetic Slices.cpp
+++ Day33-DynamicProgramming/413. Arithmetic Slices.cpp
@@ -7,6 +7,17 @@ using namespace std;
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
+        const int n = nums.size();
+        if (n <= 2) return 0;
+        int pre = 0, curr = 0, sums = 0;
+        for (int i = 2; i < n; i++) {
+            if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+                curr = pre + 1;
+                sums += curr;
+                pre = curr;
+            }
+        }
+        return sums;
     }
 
 };
