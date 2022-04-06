713d71ebd0751ba62104e63d71ef7b499badbd94
Siying Chen <chensiyingcrystal@gmail.com>
2022-04-05 18:27:01 -0700

    Update 413. Arithmetic Slices.cpp

--- Day33-DynamicProgramming/413. Arithmetic Slices.cpp
+++ Day33-DynamicProgramming/413. Arithmetic Slices.cpp
@@ -30,8 +30,8 @@ public:
         }
         return sums;
     }
-
-//暴力解法：判断每个子数组是不是等差数列
+//循序渐进找到简洁解法的步骤
+//第一步：暴力解法：判断每个子数组是不是等差数列；time: o(n3)
     int numberOfArithmeticSlices(vector<int>& nums) {
         const int n = nums.size();
         if (n <= 2) return 0;
@@ -54,5 +54,25 @@ public:
             }
         }
         return true;
+    }
+//仔细观察上面的代码，发现第二遍循环和helper function里面的执行过程有重复，为什么不可以合并呢？
+//第二步解法：双指针; time: o(n2)
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        const int n = nums.size();
+        if (n <= 2) return 0;
+        int ans = 0;
+        //这里i仍然代表头，而j代表的是尾同时也是判断等差数列的进度条k
+        //如果nums[i:j]是等差数列，则j可以继续向后移动，否则就不用考虑j后面的了
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 2; j < n; j++) {
+                if (nums[j] - nums[j - 1] == nums[j - 1] - nums[j - 2]) {
+                    ans++;
+                }
+                else {
+                    break;
+                }
+            }
+        }
+        return ans;
     }
 };
