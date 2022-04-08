fec9848e72d0a3c7ddf576cc17fd29f5bc2c568d
Siying Chen <chensiyingcrystal@gmail.com>
2022-04-07 21:28:38 -0700

    update

--- Day34-DP/64. Minimum Path Sum.cpp
+++ Day34-DP/64. Minimum Path Sum.cpp
@@ -31,25 +31,39 @@ public:
         tmp -= grid[i][j];
     }
 //自己写的dp
-    int minPathSum(vector<vector<int> >& grid) {
-        const int m = grid.size();
-        const int n = grid[0].size();
-        vector<vector<int> > dp(m, vector<int>(n));
-        dp[0][0] = grid[0][0];
+//直接在原矩阵上修改不会影响结果，因此可压缩空间至o(1)
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        if (mat.empty()) return {};
+        const int m = mat.size();
+        const int n = mat[0].size();
+        vector<vector<int> > dp(m, vector<int>(n, INT_MAX - 1));
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == 0 && j == 0) continue;
-                if (i == 0 && j != 0) {
-                    dp[i][j] = dp[i][j - 1] + grid[i][j];
-                }
-                else if (j == 0 && i != 0) {
-                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                if (mat[i][j] == 0) {
+                    dp[i][j] = 0;
                 }
                 else {
-                    dp[i][j] = grid[i][j] + min(dp[i - 1][j], dp[i][j - 1]);
+                    if (i > 0) {
+                        dp[i][j] = min(dp[i][j], dp[i - 1][j] + 1);
+                    }
+                    if (j > 0) {
+                        dp[i][j] = min(dp[i][j], dp[i][j - 1] + 1);
+                    }
                 }
             }
         }
-        return dp[m - 1][n - 1];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (mat[i][j] != 0) {
+                    if (i < m - 1) {
+                        dp[i][j] = min(dp[i][j], dp[i + 1][j] + 1);
+                    }
+                    if (j < n - 1) {
+                        dp[i][j] = min(dp[i][j], dp[i][j + 1] + 1);
+                    }
+                }
+            }
+        }
+        return dp;
     }
 };
