94cb54e82ba21376c9dac8c190f9f87b7a93fd54
Siying Chen <chensiyingcrystal@gmail.com>
2022-04-07 17:58:50 -0700

    Update 64. Minimum Path Sum.cpp

--- Day34-DP/64. Minimum Path Sum.cpp
+++ Day34-DP/64. Minimum Path Sum.cpp
@@ -30,12 +30,26 @@ public:
         if (j + 1 < grid[0].size()) backtrack(grid, i, j + 1, tmp, sums);
         tmp -= grid[i][j];
     }
-
+//自己写的dp
     int minPathSum(vector<vector<int> >& grid) {
         const int m = grid.size();
         const int n = grid[0].size();
         vector<vector<int> > dp(m, vector<int>(n));
-        
-
+        dp[0][0] = grid[0][0];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) continue;
+                if (i == 0 && j != 0) {
+                    dp[i][j] = dp[i][j - 1] + grid[i][j];
+                }
+                else if (j == 0 && i != 0) {
+                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                }
+                else {
+                    dp[i][j] = grid[i][j] + min(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 };
