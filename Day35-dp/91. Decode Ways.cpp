e533f65ae2a876e13194fc23e79c080e71079810
Siying Chen <chensiyingcrystal@gmail.com>
2022-04-08 17:31:23 -0700

    Update 91. Decode Ways.cpp

--- Day35-dp/91. Decode Ways.cpp
+++ Day35-dp/91. Decode Ways.cpp
@@ -9,17 +9,18 @@ public:
     int numDecodings(string s) {
         if (s.empty() || s[0] == '0') return 0;
         const int n = s.length();
-        vector<int> dp(n - 1);
-        dp[0] = 1;
-        for (int i = 1; i < n; i++) {
-            if (s[i - 1] == '0' || s.substr(i - 1, 2) > "27") {
-                if (s[i] == '0') return 0;
+        //这里要对dp扩容，添加虚拟的dp[0]
+        vector<int> dp(n + 1);
+        dp[0] = 1, dp[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            if (s[i - 2] == '0' || s.substr(i - 2, 2) >= "27") {
+                if (s[i - 1] == '0') return 0;
                 else {
                     dp[i] = dp[i - 1];
                 }
             }
             else {
-                if (s[i] == 0) {
+                if (s[i - 1] == '0') {
                     dp[i] = dp[i - 2];
                 }
                 else {
@@ -27,6 +28,6 @@ public:
                 }
             }
         }
-        return dp[n - 1];
+        return dp[n];
     }
 };
