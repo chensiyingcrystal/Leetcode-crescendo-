f3bab0d8a8e0f206eda656d7a8a9644e160ccb37
Siying Chen <chensiyingcrystal@gmail.com>
2022-04-13 11:50:52 -0700

    Update 1143. Longest Common Subsequence.cpp

--- Dya36-dp/1143. Longest Common Subsequence.cpp
+++ Dya36-dp/1143. Longest Common Subsequence.cpp
@@ -6,10 +6,11 @@ using namespace std;
 
 class Solution {
 public:
+//dp的第二种写法：到i、j为止，因此不需要额外max_Len做记录，最后的结果即为dp[m][n]
 //dp[i][j]represents to the index i for the text1 and j for the text2
 //the length of the longest common subsequence
 //The formula should be: 
-//dp[i][j] = dp[i-1][j-1] + 1, if text1[i] == text2[j]
+//dp[i][j] = dp[i-1][j-1] + 1, if text1[i-1] == text2[j-1]
 //else: dp[i][j] = max(dp[i-1][j], dp[i][j-1]) 
     int longestCommonSubsequence(string text1, string text2) {
         const int m = text1.length();
