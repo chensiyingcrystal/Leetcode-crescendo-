d7cbafaf5cdf19b111cf51d7d9dc3f07c975be25
Siying Chen <chensiyingcrystal@gmail.com>
2022-04-13 11:49:55 -0700

    Update 1143. Longest Common Subsequence.cpp

--- Dya36-dp/1143. Longest Common Subsequence.cpp
+++ Dya36-dp/1143. Longest Common Subsequence.cpp
@@ -12,12 +12,19 @@ public:
 //dp[i][j] = dp[i-1][j-1] + 1, if text1[i] == text2[j]
 //else: dp[i][j] = max(dp[i-1][j], dp[i][j-1]) 
     int longestCommonSubsequence(string text1, string text2) {
-
-        
-
-
-
-
-
-  }
+        const int m = text1.length();
+        const int n = text2.length();
+        vector<vector<int> > dp(m + 1, vector<int>(n + 1, 0));
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
 };
