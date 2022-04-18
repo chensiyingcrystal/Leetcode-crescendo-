b8c12a834072660cf4c699e233dea561dba09291
Siying Chen <chensiyingcrystal@gmail.com>
2022-04-18 13:00:00 -0700

    update

--- Day37-BinarySearch/34. Find First and Last Position of Element in Sorted Array.cpp
+++ Day37-BinarySearch/34. Find First and Last Position of Element in Sorted Array.cpp
@@ -14,7 +14,61 @@ public:
     }
    vector<int> searchRange(vector<int>& nums, int target) {
        if (nums.empty()) return {-1, -1};
-       const int n = nums.length();
-       int left = 0, right = n - 1;
+       const int n = nums.size();
+       int left1 = 0, right1 = n - 1;
+       //set the variable name as begin and end
+       int l = INT_MAX;
+       while (left1 <= right1) {
+           int mid1 = left1 + (right1 - left1) / 2;
+           if (nums[mid1] == target) {
+               if (mid1 == 0) {
+                   l = mid1; 
+                   break;
+               }
+               else {
+                   if (nums[mid1 - 1] == target) {
+                       right1 = mid1 - 1; //
+                   }
+                   else {
+                       l = mid1;
+                       break;
+                   }
+               }
+           }
+           else if (nums[mid1] > target) {
+               right1 = mid1 - 1;
+           }
+           else {
+               left1 = mid1 + 1;
+           }
+       }
+       if (l == INT_MAX) return {-1, -1};
+       int r;
+       int left2 = 0, right2 = n - 1;
+       while (left2 <= right2) {
+           int mid2 = left2 + (right2 - left2) / 2;
+           if (nums[mid2] == target) {
+               if (mid2 == n - 1) {
+                   r = n - 1;
+                   return {l, r};
+               }
+               else {
+                   if (nums[mid2 + 1] == target) {
+                       left2 = mid2 + 1;
+                   }
+                   else {
+                       r = mid2;
+                       return {l, r};
+                   }
+               }
+           }
+           else if (nums[mid2] > target) {
+               right2 = mid2 - 1;
+           }
+           else {
+               left2 = mid2 + 1;
+           }
+       }
+       return {l, r};
    }
 };
---
+++ My-Leetcode_notes/Binary Search/Binary-search-34.md
@@ -0,0 +1,18 @@
+# Binary Search 34
+1. Question Overview
+This question requires using two binary searches to find the first and last position of the target
+
+2. Design and Mindset
+important: overall perspective全局观
+            -before writing the code, having the eagle-view of all codes
+            -capsulate duplicate codes into the function; important abstraction skills
+Interview tips: give more information to the interviewers; try to write the first block of codes
+when detecting duplicate codes, quickly rearmed your mind and communicate with the interviewer随机应变，及时沟通
+
+3. Algorithms
+a. find the first and last position using binary search
+b. there's two cases where we encounter the first target:
+    -the mid element is the first element
+    -the element to the left of the mid index is not equal to the mid element
+c. we can set a helper function for the main function and add a boolean to decide whether we want to find the first or the last position
+                
