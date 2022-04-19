861f3828e03a6a2e6d562414b4b9b01ea68c65ec
Siying Chen <chensiyingcrystal@gmail.com>
2022-04-19 12:23:59 -0700

    Update 33. Search in Rotated Sorted Array.cpp

--- Day37-BinarySearch/33. Search in Rotated Sorted Array.cpp
+++ Day37-BinarySearch/33. Search in Rotated Sorted Array.cpp
@@ -70,31 +70,28 @@ public:
         return -1;
     }
 //简化后的代码
-        int search(vector<int>& nums, int target) {
+    int search(vector<int>& nums, int target) {
         //special case where nums has only one number
         const int n = nums.size();
         if (n == 1) {
             return target == nums[0] ? 0 : -1;
         }
-        //first find the index of the pivot
+        //if not rotated, search the entire array
         if (nums[0] < nums[n - 1]) {
             return binarySearch(nums, target, 0, n - 1);  
         }
+        //find pivot for the rotated array
         int pivot = findPivot(nums, n);
             //compare target with the value of the pivot to decide which side it is in
         if (target >= nums[0]) {
             return binarySearch(nums, target, 0, pivot);
         }
-        else if (pivot + 1 <= n - 1 && target < nums[0]) {
-            return binarySearch(nums, target, pivot + 1, n - 1);
-        }
         else {
-            return -1;
+            return binarySearch(nums, target, pivot + 1, n - 1);
         }
     }
 
     int findPivot(vector<int>& nums, int n) {
-        //----------add: if not rotated, return 0-----------
         int left = 0, right = n - 1;
         while (right - left > 1) {
             int mid = left + (right - left) / 2;
