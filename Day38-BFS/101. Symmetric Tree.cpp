7e403d12ba3d2b12eab31e6178e06620bc5ece5e
Siying Chen <chensiyingcrystal@gmail.com>
2022-04-22 14:22:55 -0700

    Update 101. Symmetric Tree.cpp

--- Day38-BFS/101. Symmetric Tree.cpp
+++ Day38-BFS/101. Symmetric Tree.cpp
@@ -18,6 +18,10 @@ public:
 //method1: bfs(iteratively)
 //time: traverse the entire tree and each node one, the total run time is O(N)
 //space: the worst case, the maximum nodes in the queue is n; O(n)
+//正向的遍历：在初始化q之前，将非空的左右子树加入q；
+            //对当前的node进行判断处理
+            //在满足一定条件后将node的child加入q
+            //继续下一轮循环直至q为空
     bool isSymmetric(TreeNode* root) {
         if (root == nullptr) return true;
         if (root -> left == nullptr && root -> right == nullptr) return true;
@@ -53,6 +57,9 @@ public:
 //method2: dfs(recursively)
 //time: o(n)
 //space: o(n); in the worst case, the tree is linear and the height is o(n)
+//递归的思想：需要建立helper function，进行两个node是否symmetric的判断，这是从root的下一层开始的
+            //base case：两个都为空；一个为空，一个不为空；两个数值不相等；都可以直接返回结果
+            //递归：由下一个bottom层级的结果决定
     bool isSymmetric(TreeNode* root) {
         if (root == nullptr) return true;
         return dfs(root -> left, root -> right);
