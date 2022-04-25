34e00eb80c11bdcde1acbcb8f937d6442a8625cd
Siying Chen <chensiyingcrystal@gmail.com>
2022-04-25 10:12:49 -0700

    Update 111. Minimum Depth of Binary Tree.cpp

--- Day39-BFS/111. Minimum Depth of Binary Tree.cpp
+++ Day39-BFS/111. Minimum Depth of Binary Tree.cpp
@@ -16,7 +16,34 @@ struct TreeNode {
 
 class Solution {
 public:
+//dfs
     int minDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+        return min(minDepth(root -> left), minDepth(root -> right)) + 1;     
+    }
+//bfs
+    int minDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+        queue<TreeNode*> q;
+        q.push(root);
         
+        int level = 0;
+        while(!q.empty()) {
+            int size = q.size();
+            while (size--) {
+                TreeNode* node = q.front();
+                //!!!!!!always remember to pop!
+                q.pop();
+
+                if (node -> left == nullptr && node -> right == nullptr) {
+                    return ++level;
+                }
+
+                if (node -> left) q.push(node -> left);
+                if (node -> right) q.push(node -> right);
+            }
+            level++;
+        } 
+        return level;
     }
 };
