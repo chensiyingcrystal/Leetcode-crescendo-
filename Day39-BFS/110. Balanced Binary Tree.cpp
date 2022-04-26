053bbcb8cb74f4bc620306ce5388251bc38ef4f5
Siying Chen <chensiyingcrystal@gmail.com>
2022-04-26 16:19:56 -0700

    Update 110. Balanced Binary Tree.cpp

--- Day39-BFS/110. Balanced Binary Tree.cpp
+++ Day39-BFS/110. Balanced Binary Tree.cpp
@@ -1,5 +1,5 @@
 #include <vector>
-#include <unordered_set>
+#include <unordered_map>
 #include <queue>
 #include <stack>
 
@@ -16,6 +16,12 @@ struct TreeNode {
 
 class Solution {
 public:
+//这道题最初写不出来是因为没有理解题意，不能很好地将题目拆分成子问题
+//这道题的特点：下层的node的结果会从两方面影响到上层的：
+             //第一，左右子树只要有任一子树不符合，母树即可不符合
+             //第二，左右子树之间需要满足条件
+//自己写的缺点：每次判断完左右子树的平衡之后，还需要重新向下递归计算一遍height
+//有没有办法能够一次遍历即可既判断平衡又计算了height呢
     bool isBalanced(TreeNode* root) {
         if (root == nullptr) return true;
         if (!isBalanced(root -> left)) return false;
@@ -26,6 +32,13 @@ public:
     int treeLevel(TreeNode* node) {
         if (node == nullptr) return 0;
         return max(treeLevel(node -> left), treeLevel(node -> right)) + 1;
+    }
+//one-way: use global variable or reference variable
+public:
+    bool isBalanced(TreeNode* root) {
+        if (root == nullptr) return true;
+        
+
     }
 
     
