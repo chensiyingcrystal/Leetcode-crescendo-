367a4e9e09216ed7088fc5ec224843463c864295
Siying Chen <chensiyingcrystal@gmail.com>
2022-04-26 23:15:04 -0700

    Update 111. Minimum Depth of Binary Tree.cpp

--- Day39-BFS/111. Minimum Depth of Binary Tree.cpp
+++ Day39-BFS/111. Minimum Depth of Binary Tree.cpp
@@ -74,6 +74,25 @@ public:
     }
 //小熊推荐的另一种写法
     int minDepth(TreeNode* root) {
-        
+        //可以先在主函数中排除root为null的特殊情况
+        if (root == nullptr) return 0;
+        return dfs(root);
+    }
+
+    int dfs(TreeNode* root) {
+        //这里root肯定不会为null，那么base case考虑叶子的情况
+        if (root -> left == nullptr && root -> right == nullptr) return 1;
+        //注意⚠️写递归方程前考虑下左或右为空会不会对结果有影响
+        //对于本题，理解题意后会发现，左右子树任一为空和两边为空时候的递归方程是不一样的
+        //重要思维：思考root这步是如何由root下面的左右两个部分得到的结果！！！
+        if (root -> left == nullptr) {
+            return dfs(root -> right) + 1;
+        }
+        else if (root -> right == nullptr) {
+            return dfs(root -> left) + 1;
+        }
+        else {
+            return min(dfs(root -> left), dfs(root -> right)) + 1;
+        }
     }
 };
