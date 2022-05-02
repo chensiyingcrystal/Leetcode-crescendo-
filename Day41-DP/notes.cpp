0f140065eee8370afb42855c134643f9b26fb9f8
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-02 15:04:36 -0700

    Create notes.cpp

---
+++ Day41-DP/notes.cpp
@@ -0,0 +1,25 @@
+# include <vector>
+
+using namespace std;
+/**0-1背包问题
+ * 输入：
+ * w(volume): [1, 3, 5, 2, 9, 6]
+ * v(value): [5, 8, 2, 7, 3, 1]
+ * W(maximum volume): 10
+*/
+int getMaxValue(vector<int>& w, vector<int>& v, int W) {
+    const int m = w.size(), n = W;
+    vector<vector<int> > dp(m + 1, vector<int>(n, 0));
+    for (int i = 0; i < m + 1; i++) {
+        for (int j = 1; j <= n; j++) {
+            int wi = w[i - 1], vi = v[i - 1];
+            if (j >= wi) {
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - wi] + vi);  
+            }
+            else {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+    return dp[m][n];
+}
