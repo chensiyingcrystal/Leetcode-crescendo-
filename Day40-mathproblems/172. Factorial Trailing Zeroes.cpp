52482b0e99a8b03488f358e8c368d6d776d8ad32
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-03 19:55:04 -0700

    update

--- Day40-mathproblems/172. Factorial Trailing Zeroes.cpp
+++ Day40-mathproblems/172. Factorial Trailing Zeroes.cpp
@@ -7,8 +7,10 @@ using namespace std;
 
 class Solution {
 public:
+//尾部的0由2x5而来，统计有几个（2，5）的完整配对，即min(2, 5)
+//2总是比5多，统计有几个5即可
     int trailingZeroes(int n) {
-        
+        return n < 5? 0 : n / 5 + trailingZeroes(n / 5);
     }
 
 };
---
+++ Day40-mathproblems/415. Add Strings.cpp
@@ -0,0 +1,49 @@
+#include <vector>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+
+
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        //set two pointers pointing at the end of two strings
+        int p1 = num1.length() - 1, p2 = num2.length() - 1;
+        //set our point for each round and result string;
+        int point = 0;
+        string result = "";
+        //begin looping
+        while (p1 >= 0 && p2 >= 0) {
+            //adding two number p1 and p2 pointing to and add point to it
+            int tmp = (num1[p1] - '0') + (num2[p2] - '0') + point;
+            //setting tmp's last number as the string's end character
+            result = to_string(tmp % 10) + result;
+            //setting tmp's first number as point for next turn;
+            point = tmp / 10;
+            //incrementing p1 and p2;
+            p1--;
+            p2--;
+        }
+
+        while (p1 >= 0) {
+            int tmp = (num1[p1] - '0') + point;
+            result = to_string(tmp % 10) + result;
+            point = tmp / 10;
+            p1--;
+        }
+
+        while (p2 >= 0) {
+            int tmp = (num2[p2] - '0') + point;
+            result = to_string(tmp % 10) + result;
+            point = tmp / 10;
+            p2--;
+        }
+        
+        if (point > 0) result = to_string(point) + result;
+        
+        return result;
+    }
+
+};
