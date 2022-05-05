eef093198ef9897a1c427d49255533cec41f9f0d
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-04 17:05:36 -0700

    update

--- Day43-DataStructures/448. Find All Numbers Disappeared in an Array.cpp
+++ Day43-DataStructures/448. Find All Numbers Disappeared in an Array.cpp
@@ -1,13 +1,37 @@
 #include <vector>
 #include <unordered_map>
-#include <string>
+#include <unordered_set>
 
 using namespace std;
 
 class Solution {
 public:
+//using unordered_map
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        
-    }
+        unordered_map<int, int> count;
+        const int n = nums.size();
 
+        for (int i = 0; i < n; i++) {
+            count[nums[i]]++;
+        }
+        vector<int> ans;
+        for (int i = 1; i <= n; i++) {
+            if (!count.count(i)) ans.push_back(i);
+        }
+        return ans;  
+    }
+//using unordered_set
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        unordered_set<int> count;
+        const int n = nums.size();
+        for (auto& num : nums) {
+            count.insert(num);
+        }
+
+        vector<int> ans;
+        for (int i = 1; i <= n; i++) {
+            if (count.find(i) == count.end()) ans.push_back(i);
+        }
+        return ans;  
+    }
 };
