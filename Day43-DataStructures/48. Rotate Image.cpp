caadd37500117ce6180383f43dfab3c3d25ecba1
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-04 18:15:17 -0700

    Update 48. Rotate Image.cpp

--- Day43-DataStructures/48. Rotate Image.cpp
+++ Day43-DataStructures/48. Rotate Image.cpp
@@ -7,6 +7,21 @@ using namespace std;
 class Solution {
 public:
     void rotate(vector<vector<int> >& matrix) {
-        
+        //first fold the matrix against the horizontal center line
+        const int m = matrix.size(), n = matrix[0].size();
+        for (int i = 0; i < m / 2; i++) {
+            for (int j = 0; j < n; j++) {
+                swap(matrix[i][j], matrix[m - 1 - i][j]);
+            }
+        }
+
+        //then fold against the diagonal line (from left to right)
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j > i) swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        return;
+
     }
 };
