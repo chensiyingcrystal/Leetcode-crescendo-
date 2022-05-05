2c2a2ed0be04f4f13c4e29f5614ca6138b5507c0
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-04 22:49:53 -0700

    Update 240. Search a 2D Matrix II.cpp

--- Day43-DataStructures/240. Search a 2D Matrix II.cpp
+++ Day43-DataStructures/240. Search a 2D Matrix II.cpp
@@ -6,49 +6,51 @@ using namespace std;
 
 class Solution {
 public:
+//这道题直觉感是使用二分，但是如何使用却需要分解问题的能力
+//错误思路：想着沿着四周分别进行二分（从左上角汇聚到右下角）但二分的使用前先存为数组（消耗时间on，得不偿失）
+//学习技巧：如何按照diag来移动（一层循环控制i就好了，因为此时row和col都为i） + 二分要用index来做！！！
     bool searchMatrix(vector<vector<int> >& matrix, int target) {
         const int m = matrix.size(), n = matrix[0].size();
-        pair<int, int> left(0, 0), right(m - 1, n - 1);
-        while (left.first <= right.first && left.second <= right.second) {
-            int low = matrix[left.first][left.second];
-            int high = matrix[right.first][right.second];
-            if (target > high || target < low) return false;
-            
-            vector<int> search1;
-            vector<int> search2;
-            for (int i = left.first, j = left.second; j <= right.second; j++) {
-                search1.push_back(matrix[i][j]);
-            }
-            for (int i = left.first + 1, j = right.second; i <= right.first; i++) {
-                search1.push_back(matrix[i][j]);
-            }
-
-            for (int j = left.second, i = left.first; i <= right.first; i++) {
-                search2.push_back(matrix[i][j]);
-            }
-            for (int i = right.first, j = left.second + 1; j <= right.second; j++) {
-                search2.push_back(matrix[i][j]);
-            }
-
-            bool ans = binarySearch(search1, target) || binarySearch(search2, target);
+        int diag = min(m, n);
+        //break into two parts! not completing one time!
+        for (int i = 0; i < diag; i++) {
+            bool ans = binarySearch(matrix, target, m, n, i, true) 
+                         || binarySearch(matrix, target, m, n, i, false);
             if (ans) return true;
-            left.first++;
-            left.second++;
-            right.first--;
-            right.second--;
+        }
+        return false;
+
+    }
+
+    bool binarySearch(vector<vector<int> >& matrix, int target, int m, int n, int i, bool horizontal) {
+        //直接用index控制low和high的变化
+        int low = i, high;
+        if (horizontal) {
+            high = n - 1; 
+        }
+        else {
+            high = m - 1;
+        }
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int mid_number;
+            if (horizontal) {
+                mid_number = matrix[i][mid];
+            }
+            else {
+                mid_number = matrix[mid][i];
+            }
+            if (mid_number == target) return true;
+            else if (mid_number > target) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
         }
         return false;
     }
+//自己的另一种思路，利用左上角点和右下角点的范围进行控制，加速判断
 
-    bool binarySearch(vector<int>& nums, int target) {
-        int size = nums.size();
-        int left = 0, right = size - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) return true;
-            if (nums[mid] > target) right = mid - 1;
-            else left = mid + 1;
-        }
-        return false; 
-    }
 };
