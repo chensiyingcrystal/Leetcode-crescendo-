172f4520342ade462c7067f6a18245635ab95a60
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-11 09:57:03 -0700

    create

--- Day45-stacksandqueues/155.MinStack.cpp
+++ Day45-stacksandqueues/155.MinStack.cpp
@@ -6,27 +6,37 @@ using namespace std;
 
 class MinStack {
 public:
-    int min_value = INT_MAX;
-    stack<int> stk;
+//the prompt requires all the operations running in constant time, i.e.O(1) time
+//need to cleverlly solve the problem: instead of creating one variable min_value for recording
+//we need to record the min_value up to this current value so far
+//in regard to storing two information, we could use array or pair type in the stack
+    stack<pair<int, int> > min_stk;
 
     MinStack() {
 
     }
     
     void push(int val) {
-        stk.push(val);
-        min_value = min(min_value, val);
+        if (min_stk.empty()) {
+            min_stk.push({val, val});
+            return;
+        }
+        
+        min_stk.push({val, min(val, min_stk.top().second)});
+        return;
     }
     
     void pop() {
+        min_stk.pop();
         
     }
     
     int top() {
+        return min_stk.top().first;
         
     }
     
     int getMin() {
-        
+        return min_stk.top().second; 
     }
 };
---
+++ Day46-DataStructures/20. Valid Parentheses.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <unordered_map>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+
+    bool isValid(string s) {
+
+    }
+};
