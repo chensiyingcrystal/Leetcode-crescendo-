3018b6ab42495ee94e40df554c9578b377d5bad5
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-11 18:16:35 -0700

    Update linkedlist_pq.cpp

--- Day47-DataStructures/linkedlist_pq.cpp
+++ Day47-DataStructures/linkedlist_pq.cpp
@@ -34,3 +34,36 @@ int peek(Node** head) {
 }
 
 //function to push according to priority
+void push(Node** head, int d, int p) {
+    Node* start = (*head);
+
+    //create a new node
+    Node* temp = newNode(d, p);
+
+    //if the start node's priority is less than that of temp
+    //we'll set the temp as the start node
+    if (start -> priority < temp -> priority) {
+        //insert new node before head
+        temp -> next = start;
+        (*head) = temp;
+    }
+    else {
+        //else we'll need to look for the first node of which the p is less than the temp's
+        Node* prev = start;
+        Node* curr = start -> next;
+        while (curr != NULL && curr -> priority >= temp -> priority) {
+            prev = curr;
+            curr = curr -> next;
+        } 
+        prev -> next = temp;
+        temp -> next = curr;
+    }
+}
+
+    //function to check if list is empty
+int isEmpty(Node** head) {
+    return (*head == NULL);
+}
+
+
+}
