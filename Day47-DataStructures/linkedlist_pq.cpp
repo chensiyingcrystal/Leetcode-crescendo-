8b711fc3cee2ea49708a57b80f209d012ca44e55
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-11 17:16:20 -0700

    Update linkedlist_pq.cpp

--- Day47-DataStructures/linkedlist_pq.cpp
+++ Day47-DataStructures/linkedlist_pq.cpp
@@ -5,3 +5,10 @@ using namespace std;
 /** Using linked list to implement priority queue */
 // time complexity: enqueue o(1); dequeue o(n); peek o(n)
 
+typedef struct node {
+    int data;
+    int priority;
+
+    struct node* next;
+
+}Node;
