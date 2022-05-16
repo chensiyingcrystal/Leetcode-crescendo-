0df7b67c9429a28d4456ca28c8364af739200a51
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-16 15:40:35 -0700

    Update 326. Power of Three.cpp

--- Day49-math-and-bitmanipulation/326. Power of Three.cpp
+++ Day49-math-and-bitmanipulation/326. Power of Three.cpp
@@ -6,8 +6,12 @@ using namespace std;
 
 class Solution {
 public:
-
     bool isPowerOfThree(int n) {
-        
+        if (n == 0) return false;
+        if (n == 1) return true;
+        //bug: communicate with interview in this case
+        if (n < 0) return false;
+        if (n % 3 != 0) return false;
+        else return isPowerOfThree(n / 3);
     }
 };
