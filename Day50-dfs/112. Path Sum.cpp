143bf6ca7a5bb3aafd2e3655387f497908390a49
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-17 10:06:48 -0700

    Update 112. Path Sum.cpp

--- Day50-dfs/112. Path Sum.cpp
+++ Day50-dfs/112. Path Sum.cpp
@@ -16,6 +16,33 @@ struct TreeNode {
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
-        
+        if (root == nullptr) return false;
+        if (root -> left == nullptr && root -> right == nullptr) return root -> val == targetSum;
+        int newtargetSum = targetSum - root -> val;
+        return hasPathSum(root -> left, newtargetSum) || hasPathSum(root -> right, newtargetSum);
+    }
+//another method: not recommended, backtracking
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (root == nullptr) return false;
+        int nodeSum = 0;
+        return backtracking(root, nodeSum, targetSum);
+    }
+
+    bool backtracking(TreeNode* root, int& nodeSum, int targetSum) {
+        if (root -> left == nullptr && root ->right == nullptr) {
+            nodeSum += root -> val;
+            return nodeSum == targetSum;
+        }
+
+        nodeSum += root -> val;
+        bool ans;
+        if (root -> left != nullptr) ans = backtracking(root -> left, nodeSum, targetSum);
+        if (ans) return true;
+        //bug: point to null pointer; must add != nullptr condition
+        if (root -> left != nullptr) nodeSum -= (root -> left) -> val;
+        if (root -> right != nullptr) ans = backtracking(root -> right, nodeSum, targetSum);
+        if (ans) return true;
+        if (root -> right != nullptr) nodeSum -= (root -> right) -> val;
+        return false;
     }
 };
