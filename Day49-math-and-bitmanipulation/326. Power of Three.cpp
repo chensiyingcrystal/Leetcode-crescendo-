9c1f78fee1f5a6d257ab00ba905017ff1690ec2d
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-16 22:02:37 -0700

    update

--- Day49-math-and-bitmanipulation/326. Power of Three.cpp
+++ Day49-math-and-bitmanipulation/326. Power of Three.cpp
@@ -1,18 +1,23 @@
 #include <vector>
 #include <unordered_map>
+#include <numeric>
 
 using namespace std;
 
 
 class Solution {
 public:
-    bool isPowerOfThree(int n) {
-        if (n == 0) return false;
-        if (n == 1) return true;
-        //bug: communicate with interview in this case
-        if (n < 0) return false;
-        if (n % 3 != 0) return false;
-        else return isPowerOfThree(n / 3);
-    }
+//log4(x) = log(x) / log(4) = 1/2 * log(x) / log(2) = 1/2 * log2(x)
+//Syntax for returning natural logarithm:
+//result = log(x)
 
+//Syntax for returning logarithm (base-10 logarithm) of the argument.
+//result = log10(x)
+    bool isPowerOfFour(int n) {
+        //bug: for float, cannot use %
+        //in c++, can use fmod, this can truncate the number/denom
+        //fmod returns a float number
+        //e.g. 5.3 % 2 == 1.3
+        return (n > 0) && fmod(log10(n) / log10(4), 1) == 0;
+    }
 };
