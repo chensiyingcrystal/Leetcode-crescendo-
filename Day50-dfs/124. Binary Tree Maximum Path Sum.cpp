9c0490fe0224510384c7b7ddb30e5e7bc5aa298d
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-18 12:39:48 -0700

    Update 124. Binary Tree Maximum Path Sum.cpp

--- Day50-dfs/124. Binary Tree Maximum Path Sum.cpp
+++ Day50-dfs/124. Binary Tree Maximum Path Sum.cpp
@@ -15,6 +15,12 @@ struct TreeNode {
 
 class Solution {
 public:
+//这道题每次思维都卡在了，最大值不一定出现在经过当前node，那这样就无法连接下一个node
+//解决办法：每次dfs return的都是左+当前节点或者右+当前节点（取两者大值和当前节点拼）
+//用一个变量记录下左+当前节点+右产生的最大值即可 
+    int maxPathSum(TreeNode* root) {
+
+    }
 
 
 };
