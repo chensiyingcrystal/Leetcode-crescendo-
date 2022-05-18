7cf39ca6823285b44b191a144e023a6d3bced05d
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-18 14:47:51 -0700

    Update 242. Valid Anagram.cpp

--- Day51-String/242. Valid Anagram.cpp
+++ Day51-String/242. Valid Anagram.cpp
@@ -5,19 +5,10 @@
 
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        
+
     }
 
 
