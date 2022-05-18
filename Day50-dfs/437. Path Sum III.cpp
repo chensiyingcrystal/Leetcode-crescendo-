edbd2a8fa601246f9a1003ad91fca4b281d773e2
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-18 10:11:12 -0700

    Update 437. Path Sum III.cpp

--- Day50-dfs/437. Path Sum III.cpp
+++ Day50-dfs/437. Path Sum III.cpp
@@ -55,4 +55,51 @@ public:
         if (root -> left != nullptr) dfs(root -> left, (long long)targetSum - root -> val, count);
         if (root -> right != nullptr) dfs(root -> right, (long long)targetSum - root -> val, count);
     }
+//小熊写的，时间复杂度o(n)
+    void dfs(TreeNode* node, int targetSum, int currentVal, 
+        int& count, unordered_map<int>& visited) {
+        if (node == nullptr) {
+            return;
+        }
+        currentVal = currentVal + node->val;
+        int delta = currentVal - targetSum;
+        if (visited.count(delta)) {
+            count += visited[delta];
+        }
+        visited[currentVal]++;
+        dfs(node->left, targetSum, currentVal, count, visited);
+        dfs(node->right, targetSum, currentVal, count, visited);
+        visited[currentVal]--;
+    } 
+
+    int pathSum(TreeNode* root, int targetSum) {
+        unordered_map<int> visited;
+        int count = 0;
+        dfs(root, targetSum, 0, count, visited);
+        return count;
+    }
+//自己写的
+    int pathSum(TreeNode* root, int targetSum) {
+        if (root == nullptr) return {};
+        int count = 0;
+        long long currVal = 0;
+        unordered_map<long long, int> map;
+        map[0] = 1;
+        backtracking(root, currVal, targetSum, count, map);
+        return count;
+    }
+
+    void backtracking(TreeNode* root, long long currVal, int targetSum, int& count, unordered_map<long long, int>& map) {
+        currVal += root -> val;
+        int delta = currVal - targetSum;
+
+        if (map.count(delta)) {
+            count += map[delta];
+        }
+        map[currVal]++;
+        if (root -> left != nullptr) backtracking(root -> left, currVal, targetSum, count, map);
+        if (root -> right != nullptr) backtracking(root -> right, currVal, targetSum, count, map);
+        //只考虑当前层，在遍历完左右子树后抹去当前值！！！之后左右子树都会放照这个模式递归进行
+        map[currVal]--;
+    }
 };
