298a1ec53708560b6301bab72f1df7699a439452
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-18 16:38:39 -0700

    update

---
+++ Day51-String/205. Isomorphic Strings.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <unordered_map>
+#include <numeric>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+
+    bool isIsomorphic(string s, string t) {
+        
+    }
+
+};
--- Day51-String/242. Valid Anagram.cpp
+++ Day51-String/242. Valid Anagram.cpp
@@ -8,7 +8,23 @@ using namespace std;
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        //special case: empty string
+        if (s.length() != t.length()) return false;
+        unordered_map<char, int> map;
+        for (auto& ch : s) {
+            map[ch]++;
+        }
 
+        for (auto& ch : t) {
+            if (!map.count(ch)) return false;
+            map[ch]--;
+        }
+
+        for (auto& pair : map) {
+            if (pair.second > 0) return false;
+        }
+
+        return true;
     }
 
 
