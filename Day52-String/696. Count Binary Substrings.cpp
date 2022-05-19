a03815c7675e2cc5e81a3d85c3f93beb95872f78
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-19 11:42:12 -0700

    Create 696. Count Binary Substrings.cpp

---
+++ Day52-String/696. Count Binary Substrings.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <unordered_map>
+#include <numeric>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+
+    int countBinarySubstrings(string s) {
+        
+    }
+
+};
