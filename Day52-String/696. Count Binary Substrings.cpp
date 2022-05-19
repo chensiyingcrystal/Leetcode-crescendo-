8c2435a235089e5e2687b47d7571edc0e91391da
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-19 13:57:53 -0700

    Update 696. Count Binary Substrings.cpp

--- Day52-String/696. Count Binary Substrings.cpp
+++ Day52-String/696. Count Binary Substrings.cpp
@@ -7,9 +7,29 @@ using namespace std;
 
 class Solution {
 public:
-
+//more intuitive method: find a "01" and "10" 
+//and then expand around it, however the time complexity is o(n2)
     int countBinarySubstrings(string s) {
-        
+        int n = s.length();
+        int count = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (s[i] == '0' && s[i + 1] == '1' || s[i] == '1' && s[i + 1] == '0') {
+                helper(s, i, count);
+            }
+        }
+        return count;
+    }
+
+    void helper(string s, int i, int& count) {
+        int n = s.length();
+        int left = i, right = i + 1;
+        char pre_left = s[left], pre_right = s[right];
+        while (left >= 0 && right < n && s[left] == pre_left && s[right] == pre_right) {
+            count++;
+            left--;
+            right++;
+        }
+        return;
     }
 
 };
