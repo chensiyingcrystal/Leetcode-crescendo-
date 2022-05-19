5524a2a91c79451061c0f7eb2d61e945a163e20d
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-19 11:16:37 -0700

    Update 647. Palindromic Substrings.cpp

--- Day52-String/647. Palindromic Substrings.cpp
+++ Day52-String/647. Palindromic Substrings.cpp
@@ -32,4 +32,30 @@ public:
         return count;
     }
 
+    int countSubstrings(string s) {
+        int n = s.length();
+        vector<vector<int> > valid(2, vector<int>(n));
+        int count = 0;
+
+        for (int len = 1; len <= n; len++) {
+            for (int start = 0; len + start - 1 < n; start++) {
+                int end = start + len - 1;
+                if (len == 1) {
+                    valid[0][start] = 1;
+                }
+                else if (len == 2) {
+                    if (s[start] == s[start + 1]) valid[1][start] = 1;
+                    else valid[1][start] = 0;
+                }
+                else {
+                    if (s[start] == s[end]) valid[(len - 1) % 2][start] = valid[(len - 1) % 2][start + 1];
+                    else valid[len % 2][start] = 0;
+                }
+                count += valid[(len - 1) % 2][start];
+            }
+        }
+        return count;
+    }
+
+
 };
