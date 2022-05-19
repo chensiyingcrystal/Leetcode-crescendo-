79afe81687e3f479882b0e22437ac54035370080
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-18 17:02:25 -0700

    Update 205. Isomorphic Strings.cpp

--- Day51-String/205. Isomorphic Strings.cpp
+++ Day51-String/205. Isomorphic Strings.cpp
@@ -9,7 +9,16 @@ class Solution {
 public:
 
     bool isIsomorphic(string s, string t) {
-        
+        unordered_map<char, char> map;
+        for (int i = 0; i < s.length(); i++) {
+            if (map.count(s[i])) {
+                if (map[s[i]] != t[i]) return false;
+            }
+            else {
+                map[s[i]] = t[i];
+            }
+        }
+        return true;
     }
 
 };
