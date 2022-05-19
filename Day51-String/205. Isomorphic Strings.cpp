42706ed7f41dde3e1923d99bb91756d41141fb2c
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-18 23:48:22 -0700

    Update 205. Isomorphic Strings.cpp

--- Day51-String/205. Isomorphic Strings.cpp
+++ Day51-String/205. Isomorphic Strings.cpp
@@ -7,9 +7,17 @@ using namespace std;
 
 class Solution {
 public:
-
+//This question requires using helper function
+//The map direction is from left to right, one left to one right, no one character to two different characters
+//In the other way, to check if no two different characters to one character, 
+//we have to reverse s and t in another function
     bool isIsomorphic(string s, string t) {
+        return helper(s, t) && helper(t, s);
+    }
+
+    bool helper(string s, string t) {
         unordered_map<char, char> map;
+
         for (int i = 0; i < s.length(); i++) {
             if (map.count(s[i])) {
                 if (map[s[i]] != t[i]) return false;
@@ -18,6 +26,7 @@ public:
                 map[s[i]] = t[i];
             }
         }
+
         return true;
     }
 
