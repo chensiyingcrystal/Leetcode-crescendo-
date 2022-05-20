3d899599b846762495398b39de1e91c84ed497f3
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-19 19:55:56 -0700

    Update 227. Basic Calculator II.cpp

--- Day52-String/227. Basic Calculator II.cpp
+++ Day52-String/227. Basic Calculator II.cpp
@@ -2,6 +2,7 @@
 #include <unordered_map>
 #include <numeric>
 #include <string>
+#include <iostream>
 
 using namespace std;
 
@@ -13,34 +14,92 @@ public:
     }
 
     int helper(string s, int left, int right) {
+        int m = category(s, left, right);
+        if (m == 0) {
+            while (left <= right && (s[left] == ' ' || s[right] == ' ')) {
+                if (s[left] == ' ') left++;
+                if (s[right] == ' ') right--;
+            }
+            return stoi(s.substr(left, right - left + 1));
+        }
+
+        if (m == 1) {
+            return flat_calculator(s, left, right);
+        }
+
+        if (m == 2) {
+            return mixed_calculator(s, left, right);
+        }
+
+        return 0;
+    }
+
+    int flat_calculator(string s, int left, int right) {
+        for (int i = right; i >= left; i--) {
+            if (s[i] == '+') {
+                return helper(s, left, i - 1) + helper(s, i + 1, right);
+            }
+            if (s[i] == '-') {
+                return helper(s, left, i - 1) - helper(s, i + 1, right);
+            }
+            if (s[i] == '*') {
+                return helper(s, left, i - 1) * helper(s, i + 1, right);
+            }
+            if (s[i] == '/') {
+                return helper(s, left, i - 1) / helper(s, i + 1, right);
+            }
+        }
+        return 0;
+
+    }
+
+    int mixed_calculator(string s, int left, int right) {
         for (int i = left; i <= right; i++) {
-            if (s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/') {
-                int left_result = helper(s, left, i - 1);
-                int right_result = helper(s, i + 1, right);
-                switch(s[i]) {
-                    case('+'):
-                        return left_result + right_result;
-                    case('-'):
-                        return left_result - right_result;
-                    case('*'):
-                        return left_result * right_result;
-                    case('/'):
-                        return left_result / right_result;
-                }
+            if (s[i] == '+') {
+                return helper(s, left, i - 1) + helper(s, i + 1, right);
+            }
+        }
+        for (int i = left; i <= right; i++) {
+            if (s[i] == '-') {
+                return helper(s, left, i - 1) - helper(s, i + 1, right);
             }
         }
 
-        while (left <= right && (s[left] == ' ' || s[right] == ' ')) {
-            if (s[left] == ' ') left++;
-            if (s[right] == ' ') right--;
+        for (int i = left; i <= right; i++) {
+            if (s[i] == '*') {
+                return helper(s, left, i - 1) * helper(s, i + 1, right);
+            }
         }
 
-        return stoi(s.substr(left, right - left + 1));
+        for (int i = left; i <= right; i++) {
+            if (s[i] == '/') {
+                return helper(s, left, i - 1) / helper(s, i + 1, right);
+            }
+        }
+        return 0;
     }
 
+    int category(string s, int left, int right) {
+        bool flag1 = false, flag2 = false, flag3 = false, flag4 = false;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '+') flag1 = true;
+            if (s[i] == '-') flag2 = true;
+            if (s[i] == '*') flag3 = true;
+            if (s[i] == '/') flag4 = true;
+        }
+        if (flag1 && flag2 || flag3 && flag4) return 1;
+        if (!flag1 && !flag2 && !flag3 && !flag4) return 0;
+        else return 2;
+    }
+    
+            
 
 };
 
 int main() {
-    
+    Solution* ss = &Solution();
+    string s = "3+2*2";
+    int ans = ss->calculate(s);
+    cout << ans << endl;
+    return 0;
 }
