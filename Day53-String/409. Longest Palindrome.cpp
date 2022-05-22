79b87e25c3e8eb2e65badd851188825babc43297
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-21 20:18:00 -0700

    Update 409. Longest Palindrome.cpp

--- Day53-String/409. Longest Palindrome.cpp
+++ Day53-String/409. Longest Palindrome.cpp
@@ -8,7 +8,42 @@ using namespace std;
 
 class Solution {
 public: 
+//primary edition
     int longestPalindrome(string s) {
-        
+        unordered_map<char, int> map;
+        for (int i = 0; i < s.length(); i++) {
+            map[s[i]]++;
+        }
+
+        int sum = 0;
+        bool flag = true;
+        for (auto& pair : map) {
+            if (pair.second % 2 == 0) {
+                sum += pair.second;
+            }
+            if (pair.second % 2 != 0) {
+                sum += pair.second - 1;
+                flag = false;
+            }
+        }
+        return flag? sum : sum + 1;
+    }
+//optimized edition
+    int longestPalindrome(string s) {
+        unordered_map<char, int> map;
+        for (int i = 0; i < s.length(); i++) {
+            map[s[i]]++;
+        }
+
+        int sum = 0;
+        bool flag = true;
+        for (auto& pair : map) {
+            //optimize here
+            sum += pair.second / 2 * 2;
+            if (pair.second % 2 != 0) {
+                flag = false;
+            }
+        }
+        return flag? sum : sum + 1;
     }
 };
