7ae0db51b3e71e551e1e459b1972136688f84d4e
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-23 22:48:16 -0700

    Update 474. Ones and Zeroes.cpp

--- Day54-Dp/474. Ones and Zeroes.cpp
+++ Day54-Dp/474. Ones and Zeroes.cpp
@@ -8,6 +8,21 @@ using namespace std;
 class Solution {
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
-        
+        //1. preprocessing the vector and get the number of 1s in each element and the total mount of characters in each string
+        const int n = strs.size();
+        vector<int> length(n);
+        vector<int> ones(n);
+        for (int i = 0; i < n; i++) {
+            length[i] = strs[i].length();
+
+            int one_count = 0;
+            for (auto& c : strs[i]) {
+                if (c == '1') one_count++;
+            }
+            ones[i] = one_count;
+        }
+
+        //2. build the dynamic programming model
+        //
     }
 };
