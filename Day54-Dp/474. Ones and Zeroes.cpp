f3ed6866ad2e9538104e705d5e314c62fcfdbd2c
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-26 10:14:19 -0700

    Update 474. Ones and Zeroes.cpp

--- Day54-Dp/474. Ones and Zeroes.cpp
+++ Day54-Dp/474. Ones and Zeroes.cpp
@@ -23,6 +23,26 @@ public:
         }
 
         //2. build the dynamic programming model
-        //
+        //objective: find the largest number of elements while meeting the following requirements:
+        //first: the total characters in this string subset is 8
+        //second: the largest number of 1s in this subset is 3
+        //dp[i][j] represents the number of 1s and the largest number of elements in the subset that meets the above requirements
+        //w[i] represents the number of characters of a string;
+        //v[i] represents the number of '1' in the string
+
+        vector<pair<int, int> > dp(9);
+        dp[0] = make_pair(0, 0);
+        for (int i = 1; i < n + 1; i++) {
+            int w = length[i - 1];
+            int v = ones[i - 1];
+            for (int j = 8; j >= w; j--) {
+                if (dp[j - w].first + v <= 3) {
+                    if (dp[j - w].second > dp[j].second) {
+                        dp[j] = dp[j - w];
+                    }
+                }
+            }
+        }
+        return dp[8].second;
     }
 };
