05c1c0bda2c81c20d9e3e254766e626c00b9ee69
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-30 12:30:10 -0700

    Update 24. Swap Nodes in Pairs.cpp

--- Day55-linkedlist/24. Swap Nodes in Pairs.cpp
+++ Day55-linkedlist/24. Swap Nodes in Pairs.cpp
@@ -24,16 +24,22 @@ public:
         ListNode* curr = head, *next, *prev = dummy;
 
         while (curr != NULL && curr->next != NULL) {
+            //first store the next node that to be processed
             next = curr->next->next;
+            //then swap two nodes
             curr->next->next = curr;
+            //then let prev node point to the new current node
             prev->next = curr->next;
+            //lastly increment previous node and current node
             prev = curr;
             curr = next;
         }
-        
-        if (curr != NULL && curr->next == NULL) prev->next = curr;
-        if (curr == NULL) prev->next = NULL;
-
+        // //if there are odd number of nodes in the list
+        // if (curr != NULL && curr->next == NULL) prev->next = curr;
+        // //if there are even number of nodes in the list
+        // if (curr == NULL) prev->next = NULL;
+        //simplify above lines of codes here below:
+        prev->next = curr;
         return dummy->next;
     }
 };
