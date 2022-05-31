cec325b92aa2e20f9707fa76eec3e102d971cb38
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-31 14:12:21 -0700

    Update 234. Palindrome Linked List.cpp

--- Day55-linkedlist/234. Palindrome Linked List.cpp
+++ Day55-linkedlist/234. Palindrome Linked List.cpp
@@ -18,6 +18,33 @@ struct ListNode {
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
+        //1. traverse through the list and record the value of each node
+        string s = "";
+        ListNode* curr = head;
+        while (curr != NULL) {
+            s += to_string(curr->val);
+            curr = curr->next;
+        }
+
+        //2. determin if the value string can compose of palindrome or not
+        int start = 0, end = s.length() - 1;
+        while (start <= end) {
+            if (s[start] != s[end]) return false;
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+//o(1) space complexity solution
+    bool isPalindrome(ListNode* head) {
+        //1. find the middle node using fast and slow pointers technique
         
+
+
+        //2. reverse the half of the list
+
+
+        //3. compare the two halves of the list to determine if it's a palindrome
     }
 };
