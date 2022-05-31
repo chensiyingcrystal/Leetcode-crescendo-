77ba45d0b09348719fd2f5448884d35b1bbc9acf
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-31 16:17:26 -0700

    Create 19. Remove Nth Node From End of List.cpp

---
+++ Day55-linkedlist/19. Remove Nth Node From End of List.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <unordered_set>
+#include <stack>
+#include <string>
+#include <queue>
+
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+    }
+};
