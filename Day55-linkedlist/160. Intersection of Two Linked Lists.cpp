02e7921430661a6ea374b2ba463d2ad1d8031903
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-31 11:51:59 -0700

    update

--- Day55-linkedlist/160. Intersection of Two Linked Lists.cpp
+++ Day55-linkedlist/160. Intersection of Two Linked Lists.cpp
@@ -39,5 +39,14 @@ public:
 //if they are traveling at the same speed, then they must meet with each other
 //at the point where they have traveling same length.
 //two pointers and math to solve lots of linked list problems
-
+//In the case lists do not intersect, pA and pB will travel m+n length of nodes
+//and then both reach the end which is null, then the loop should terminate
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* pA = headA, *pB = headB;
+        while (pA != pB) {
+            pA = pA? pA->next : headB;
+            pB = pB? pB->next : headA;
+        }
+        return pA;
+    }
 };
