81046c268f2f953eb5bace1233581cbd2131f4fb
Siying Chen <chensiyingcrystal@gmail.com>
2022-06-14 11:56:07 -0700

    Create 104. Maximum Depth of Binary Tree.cpp

---
+++ Day56-Tree-Recursion/104. Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,27 @@
+#include <unordered_map>
+#include <queue>
+#include <sstream>
+
+using namespace std;
+
+class Solution {
+public:
+    Solution() {
+
+
+    }
+
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+
+    int maxDepth(TreeNode* root) {
+        if (root == NULL) return 0;
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
