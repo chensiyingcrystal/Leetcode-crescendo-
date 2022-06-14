1f92d5e43ad7505b0396403bca8d9ea887dd230d
Siying Chen <chensiyingcrystal@gmail.com>
2022-06-14 12:04:50 -0700

    Update 104. Maximum Depth of Binary Tree.cpp

--- Day56-Tree-Recursion/104. Maximum Depth of Binary Tree.cpp
+++ Day56-Tree-Recursion/104. Maximum Depth of Binary Tree.cpp
@@ -19,9 +19,32 @@ public:
         TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
         TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
     };
-
+//recursion: 
+//base case: null
+//recursive equation
+//time: o(n); space: o(n)
     int maxDepth(TreeNode* root) {
         if (root == NULL) return 0;
         return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+//b
+        int maxDepth(TreeNode* root) {
+        if (root == NULL) return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        int level = 0;
+        
+        while (!q.empty()) {
+            int k = q.size();
+            while (k--) {
+                TreeNode* tmp = q.front();
+                q.pop();
+                if (tmp->left) q.push(tmp->left);
+                if (tmp->right) q.push(tmp->right);
+            }
+            level++;
+        }
+        
+        return level;
     }
 };
