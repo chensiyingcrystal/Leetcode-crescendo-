57a6a8070cea54873999e8ff61ff4da78e433691
Siying Chen <chensiyingcrystal@gmail.com>
2022-06-15 10:10:13 -0700

    Update 543. Diameter of Binary Tree.cpp

--- Day56-Tree-Recursion/543. Diameter of Binary Tree.cpp
+++ Day56-Tree-Recursion/543. Diameter of Binary Tree.cpp
@@ -21,6 +21,16 @@ public:
     };
 
     int diameterOfBinaryTree(TreeNode* root) {
-        
+        int max_length = 0;
+        helper(root, max_length);
+        return max_length;
+    }
+    
+    int helper(TreeNode* root, int& max_length) {
+        if (root == NULL) return -1;
+        int left = helper(root->left, max_length);
+        int right = helper(root->right, max_length);
+        max_length = max(max_length, left + right + 2);
+        return max(left, right) + 1;
     }
 };
