ca80ea629319549dd2730eded810e0adc961d496
Siying Chen <chensiyingcrystal@gmail.com>
2022-06-14 18:43:51 -0700

    Create 559. Maximum Depth of N-ary Tree.cpp

---
+++ Day56-Tree-Recursion/559. Maximum Depth of N-ary Tree.cpp
@@ -0,0 +1,30 @@
+#include <unordered_map>
+#include <queue>
+#include <sstream>
+
+using namespace std;
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        
+    }
+};
+
