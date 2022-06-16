d69f829d2844437711889d43cdc90d8b60084122
Siying Chen <chensiyingcrystal@gmail.com>
2022-06-15 22:52:49 -0700

    Update 437. Path Sum III.cpp

--- Day57-tree-dfs/437. Path Sum III.cpp
+++ Day57-tree-dfs/437. Path Sum III.cpp
@@ -23,6 +23,7 @@ public:
     int pathSum(TreeNode* root, int targetSum) {
         if (root == NULL) return 0;
         unordered_map<int, int> map;
+        //important: initializing the map; what if the root value is equal with targetSum?
         map[0] = 1;
         int count = 0;
         dfs(root, targetSum, 0, map, count);
@@ -31,7 +32,7 @@ public:
     
     void dfs(TreeNode* root, int targetSum, long nodeSum, unordered_map<int, int>& map, int& count) {
         if (root == NULL) return;
-        
+        //important: keep the sum of nodes' val in the limitation of int
         nodeSum += (long)root->val;
         if (map.find(nodeSum - targetSum) != map.end()) {
             count += map[nodeSum - targetSum];
@@ -40,6 +41,7 @@ public:
         
         dfs(root->left, targetSum, nodeSum, map, count);
         dfs(root->right, targetSum, nodeSum, map, count);
+        //important: must erase nodeSum from the map before going into the parellel subtree
         map[nodeSum]--;
     }
 };
