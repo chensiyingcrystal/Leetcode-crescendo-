d8aab75b38500c3b15016383c5d7ac2cfc840d40
Siying Chen <chensiyingcrystal@gmail.com>
2022-06-15 22:58:18 -0700

    Create 1110. Delete Nodes And Return Forest.cpp

---
+++ Day57-tree-dfs/1110. Delete Nodes And Return Forest.cpp
@@ -0,0 +1,50 @@
+#include <unordered_map>
+#include <queue>
+#include <sstream>
+
+using namespace std;
+
+class Solution {
+public:
+    Solution() {
+
+
+    }
+
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        //1. preprocessing: convert to_delete to hashset to do o(1) search each time
+        unordered_set<int> delete_set;
+        for (int& num : to_delete) {
+            delete_set.insert(num);
+        }
+        
+        //2. dfs finding all islands and add them to the result
+        vector<TreeNode*> ans;
+        dfs(root, ans, true, delete_set);
+        return ans;
+        
+    }
+    
+    void dfs(TreeNode*& root, vector<TreeNode*>& ans, bool flag, unordered_set<int>& delete_set) {
+        if (root == NULL) return;
+        if (delete_set.find(root->val) != delete_set.end()) {
+            dfs(root->left, ans, true, delete_set);
+            dfs(root->right, ans, true, delete_set);
+            root = NULL;
+        }
+        else {
+            dfs(root->left, ans, false, delete_set);
+            dfs(root->right, ans, false, delete_set);
+            if (flag) ans.push_back(root);
+        }
+    }
+};
