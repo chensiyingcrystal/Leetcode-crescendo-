ae85a077a116212682e67ed938e3745758ccf532
Siying Chen <chensiyingcrystal@gmail.com>
2022-06-17 10:34:55 -0700

    Create 105. Construct Binary Tree from Preorder and Inorder Traversal.cpp

---
+++ Day58-Tree-traversal/105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -0,0 +1,58 @@
+#include <unordered_map>
+#include <queue>
+#include <sstream>
+#include <stack>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    Solution() {
+
+
+    }
+
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int m = preorder.size();
+        int n = inorder.size();
+        return helper(preorder, 0, m - 1, inorder, 0, n - 1);
+    }
+
+    TreeNode* helper(vector<int>& preorder, int pre_start, int pre_end, vector<int>& inorder, int in_start, int in_end) {
+        if (pre_start == pre_end && in_start == in_end) {
+            TreeNode* temp = (TreeNode*)malloc(sizeof(TreeNode));
+            temp = &TreeNode(preorder[pre_start]);
+            return temp;
+        }
+        
+        int pivot;
+        for (int i = in_start; i <= in_end; i++) {
+            if (inorder[i] == preorder[pre_start]) {
+                pivot = i; 
+                break;
+            }
+        }
+
+        TreeNode* root = (TreeNode*)malloc(sizeof(TreeNode));
+        root = &TreeNode(preorder[pre_start]);
+        
+        int left_number = pivot - in_start;
+        int right_number = in_end - pivot;
+
+        root->left = helper(preorder, pre_start + 1, pre_start + left_number, inorder, in_start, pivot - 1);
+        root->right = helper(preorder, pre_start + left_number + 1, pre_end, inorder, pivot + 1, in_end);
+        return root;
+    }
+
+};
