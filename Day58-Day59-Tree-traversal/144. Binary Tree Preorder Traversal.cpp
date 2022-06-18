4f2f188d485ed4c8d1321ce786bc15087694a86b
Siying Chen <chensiyingcrystal@gmail.com>
2022-06-18 12:35:46 -0700

    Update 144. Binary Tree Preorder Traversal.cpp

--- Day58-Day59-Tree-traversal/144. Binary Tree Preorder Traversal.cpp
+++ Day58-Day59-Tree-traversal/144. Binary Tree Preorder Traversal.cpp
@@ -40,6 +40,17 @@ public:
 //Since the dfs is actually based on calling recursive stack, we can use stack to implement it iteratively
     vector<int> preorderTraversal(TreeNode* root) {
         if (root == NULL) return {};
+        vector<int> ans;
+        stack<TreeNode*> stk;
+        stk.push(root);
         
+        while (!stk.empty()) {
+            TreeNode* temp = stk.top();
+            stk.pop();
+            ans.push_back(temp->val);
+            if (temp->right != NULL) stk.push(temp->right);
+            if (temp->left != NULL) stk.push(temp->left);
+        }
+        return ans;
     }
 };
