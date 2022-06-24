6507cafc628cf875f8206777cb01bdcd6dd18796
Siying Chen <chensiyingcrystal@gmail.com>
2022-06-23 22:38:35 -0700

    Update 301. Remove Invalid Parentheses.cpp

--- Day62-DP/301. Remove Invalid Parentheses.cpp
+++ Day62-DP/301. Remove Invalid Parentheses.cpp
@@ -14,11 +14,12 @@ public:
 
     }
 //The first time solution: naive backtrack(tle)
+//非常多的不必要的步骤，代码结构不清晰，时间复杂度高
     vector<string> removeInvalidParentheses(string s) {
         unordered_set<string> ans;
         string temp = "";
         backtrack(s, temp, ans, 0);
-        
+        //after getting the answer we select the elements with lease removed characters
         unordered_map<int, vector<string>> map;
         int min_diff = INT_MAX;
         for (auto& new_s : ans) {
@@ -44,11 +45,17 @@ public:
             backtrack(s, temp, ans, first + 1);
         }
         else {
+            //!!!!!!!!!IMPORTANT BUG!!!!!!!!!
+            //Here we say if it's a character, then we always count it and not discard it
+            //However, we still need to pop it back because when it go back to its parent level
+            //it should disappear otherwise in the parent level the first element is not
+            //a character expected but this lower case letter; and when it pop back, what gets
+            //popped back is this lower case letter but not the parentheses as expected.
             temp.pop_back();
         }
         
     }
-    
+//here we use a stack solution to figure out a string is a valid parentheses
     bool isValidString(string temp) {
         if (temp == "") return true;
         stack<char>stk;
