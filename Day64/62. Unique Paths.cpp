7b448aadc1f2c1e597d5d68bcc2ddb37ac54d726
Siying Chen <chensiyingcrystal@gmail.com>
2022-07-18 11:38:14 -0700

    Update 62. Unique Paths.cpp

--- Day64/62. Unique Paths.cpp
+++ Day64/62. Unique Paths.cpp
@@ -12,6 +12,16 @@ public:
 
 
     }
+//why we can use dynamic programming here?
+    //initially, we can think of a recursion method or, backtracking 
+    //however, if we use them, we will do a bunch of duplicate work
+    //so we think about using memorization!! And that is how dp forms.
+//MORE quick way to think of dp
+   //observe data input size: m, n <= 100, we cannot use recursive ways 
+//Some constraints if we can use dp
+   //two directions, no opportunity to go back and forth, maintain in an order!
+   //count unique paths instead of outputting all unique paths.
+
     int uniquePaths(int m, int n) {
         vector<vector<int> > dp(m, vector<int>(n)); //dp: unique paths from starting point to destination
         dp[0][0] = 1; // base case
