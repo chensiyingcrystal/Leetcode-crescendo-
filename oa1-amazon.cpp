6858440140b523196ae36d06b9b5b9aebb97e3d1
Siying Chen <chensiyingcrystal@gmail.com>
2022-07-18 19:43:19 -0700

    Create oa1-amazon.cpp

---
+++ oa1-amazon.cpp
@@ -0,0 +1,43 @@
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <string.h>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    Solution() {
+
+
+    }
+
+    int findMaxObservableSum(vector<int>& nums, int k) {
+        unordered_map<int, int> map;
+        int ans = -1, sum = 0, count = 0, left = 0, right = 0;
+        const int n = nums.size();
+
+        while (right <= n - k) {
+            sum += nums[right];
+            count++;
+            right++;
+            map[right]++;
+
+            while (map[right] > 1) {
+                map[left]--;
+                left++;
+            }
+            
+            if (count == k) {
+                ans = max(ans, sum);
+                sum = 0;
+                count = 0;
+            }
+        }
+
+        return ans;
+
+
+    }
+};
