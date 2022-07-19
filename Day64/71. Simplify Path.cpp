a38a2136cd007882c709e1a75ea1712855244a64
Siying Chen <chensiyingcrystal@gmail.com>
2022-07-18 17:11:02 -0700

    Update 71. Simplify Path.cpp

--- Day64/71. Simplify Path.cpp
+++ Day64/71. Simplify Path.cpp
@@ -21,6 +21,9 @@ public:
 //data structure: stack
 //how to process the string: first skip the "/", add the string into stack instead of characters, finally construct ans using valid strings in the stack
 //reversely read the string from stk: use string concatenation
+
+//Time Complexity: O(N)O(N) if there are NN characters in the original path. First, we spend O(N)O(N) trying to split the input path into components and then we process each component one by one which is again an O(N)O(N) operation. We can get rid of the splitting part and just string together the characters and form directory names etc. However, that would be too complicated and not worth depicting in the implementation. The main idea of this algorithm is to use a stack. How you decide to process the input string is a personal choice.
+//Space Complexity: O(N)O(N). Actually, it's 2N2N because we have the array that contains the split components and then we have the stack.
     string simplifyPath(string path) {
         //loop through our path and store the valid string in the stack
           //rule1: igonre the '/'
