7acc4bddc7a7e3f5f7d3902c90013ac5bd204c76
Siying Chen <chensiyingcrystal@gmail.com>
2022-07-19 14:09:17 -0700

    Update s1.cpp

--- Day65-sliding_window/s1.cpp
+++ Day65-sliding_window/s1.cpp
@@ -13,5 +13,26 @@ public:
 
     }
 
+//给定一个整数数组，计算长度为k的连续子数组的最大总和
+//brute force
+    int findkMaxSum(vector<int>& nums, int k) {
+        int n = nums.size();
+        int sum = 0, start = nums[0];
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+
+        int maxSum = sum;
+        for (int i = 1; i + k - 1 < n; i++) {
+            sum = sum - start + nums[i + k - 1];
+            maxSum = max(maxSum, sum);
+        }
+
+        return maxSum;
+    }
+//sliding window
+    int findkMaxSum(vector<int>& nums, int k) {
+
+    }
     
 };
