f870514dedba28fb22614fd4ce57d8fdf50a12cb
Siying Chen <chensiyingcrystal@gmail.com>
2022-07-26 13:45:19 -0700

    Update 76. Minimum Window Substring.cpp

--- Day66-slidingwindow-collection/76. Minimum Window Substring.cpp
+++ Day66-slidingwindow-collection/76. Minimum Window Substring.cpp
@@ -49,5 +49,41 @@ public:
 
         return minLength == INT_MAX? "" : s.substr(start, minLength);
     }
+//用vector<int>的优化
+    string minWindow(string s, string t) {
+        int m = s.length(), n = t.length();
+        if (m < n) return "";
 
+        int left = 0, right = 0;
+        vector<int> freq_t(128, 0);
+        vector<int> freq_s(128, 0); //record each character's frequency
+        int count = 0, need = 0; // record the number of valid character
+        for (char& c : t) {
+            if (freq_t[c] == 0) need++;
+            freq_t[c]++;
+        }
+        int minLength = INT_MAX, start = 0;
+
+        while (right < m) {
+            char c = s[right];
+            freq_s[c]++;
+            if (freq_t[c] != 0 && freq_t[c] == freq_s[c]) count++;
+
+            while (count == need) {
+                if (right - left + 1 < minLength) {
+                    start = left;
+                    minLength = right - left + 1;
+                }
+
+                char c1 = s[left];
+                if (freq_t[c1] != 0 && freq_s[c1] == freq_t[c1]) count--;
+                freq_s[c1]--;
+                left++;
+            }
+
+            right++;
+    }
+
+        return minLength == INT_MAX? "" : s.substr(start, minLength);
+    }
 };
