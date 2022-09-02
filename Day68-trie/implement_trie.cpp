5cca37f59f0aab66bf10b45e33576c4e225f94ad
Siying Chen <chensiyingcrystal@gmail.com>
2022-09-02 16:51:19 -0700

    Update implement_trie.cpp

--- Day68-trie/implement_trie.cpp
+++ Day68-trie/implement_trie.cpp
@@ -2,23 +2,74 @@
 
 using namespace std;
 
+
+
 template<typename V>
-class TrieNode {
-public:
-    V val;
-    TrieNode* children[256];
-
-    TrieNode() {
-        val = NULL;
-        for (int i = 0; i < 256; i++) {
-            children[i] = NULL;
-        }
-    }
-};
-
 class TrieMap {
+private:
+    const int R;
+    int size;
+
+    class TrieNode {
+    public:
+        V val;
+        TrieNode* children[256];
+
+        TrieNode() {
+            val = NULL;
+            for (int i = 0; i < 256; i++) {
+                children[i] = NULL;
+            }
+        }
+    };
+
+    TrieNode* root;
+
+
 public:
     void add(string key, V val) {
 
     }
+
+    void remove(string key) {
+
+    }
+
+    void V get(string key) {
+
+    }
+
+    bool findKey(string key) {
+
+    }
+
+    string findShortestPrefix(string query) {
+
+    }
+
+    string findLongestPrefix(string query) {
+
+    }
+
+    vector<string> findAllPrefixKey(string prefix) {
+
+    }
+
+    bool findPrefixKey(string prefix) {
+
+    }
+
+    vector<string> findAllPatternKey(string pattern) {
+
+    }
+
+    bool findPatternKey(string pattern) {
+
+    }
+
+    int getSize() {
+        return size;
+    }
+
+
 };
